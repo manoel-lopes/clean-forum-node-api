2d15a2ccd7426f07c5b7d9e8b39bec4fa9c55746
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-07 11:53:19 -0300

    config: add TypeORM data source configuration for PostgreSQL

---
+++ src/infra/persistence/typeorm/datasource.ts
@@ -0,0 +1,21 @@
+import { env } from '@/lib/env'
+import { DataSource } from 'typeorm'
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  username: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
+  synchronize: true,
+  logging: env.NODE_ENV !== 'production',
+  entities: ['src/infra/persistence/typeorm/data-mappers/**/*.mapper.ts'],
+  migrations: ['src/infra/persistence/typeorm/migrations'],
+})
+
+AppDataSource.initialize()
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
