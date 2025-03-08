433b63b3085b4cd93d50f6ac04b795390d4f1f4e
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-08 17:50:50 -0300

    fix: makeAnswerComment to use AnswerComment data mapper

--- src/util/factories/domain/make-answer-comment.ts
+++ src/util/factories/domain/make-answer-comment.ts
@@ -1,13 +1,15 @@
-import { Comment } from '@/domain/entities/comment/comment.entity'
-import type { AnswerComment } from '@/infra/persistence/typeorm/data-mappers/answer-comment/answer-comment.mapper'
+import {
+  AnswerComment
+} from '@/infra/persistence/typeorm/data-mappers/answer-comment/answer-comment.mapper'
 
 export function makeAnswerComment (
   answerId: string,
   override: Partial<AnswerComment> = {}
 ): AnswerComment {
-  const comment = Comment.create({
-    content: 'any_question_content',
+  const comment = AnswerComment.create({
+    content: 'any_answer_content',
     authorId: 'any_author_id',
+    answerId: 'any_answer_id'
   })
   return Object.assign(comment, { answerId, ...override })
 }
