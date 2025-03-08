4d69444cf9dc292bbf5fca07256b71fed1bf7229
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-08 11:47:34 -0300

    refactor: streamline where clause usage in BaseDrizzleRepository methods

--- src/infra/persistence/repositories/drizzle/base/base-drizzle.repository.ts
+++ src/infra/persistence/repositories/drizzle/base/base-drizzle.repository.ts
@@ -15,28 +15,25 @@ export abstract class BaseDrizzleRepository<Table extends PgTable> {
   }
 
   async findOne ({ where }: FindOptions<Table>): Promise<InferSelectModel<Table> | null> {
-    const whereClause = this.buildWhereClause(where)
     const [entity] = await db.select()
       .from(this.table as PgTable)
-      .where(and(...whereClause))
+      .where(and(...this.buildWhereClause(where)))
       .limit(1) as InferSelectModel<Table>[]
 
     return entity ?? null
   }
 
   async deleteOne ({ where }: FindOptions<Table>): Promise<void> {
-    const whereClause = this.buildWhereClause(where)
-    await db.delete(this.table).where(and(...whereClause))
+    await db.delete(this.table).where(and(...this.buildWhereClause(where)))
   }
 
   async updateOne (
     { where }: FindOptions<Table>,
     data: Partial<InferInsertModel<Table>>
   ): Promise<InferSelectModel<Table>> {
-    const whereClause = this.buildWhereClause(where)
     const [updatedEntity] = await db.update(this.table)
       .set({ ...data })
-      .where(and(...whereClause))
+      .where(and(...this.buildWhereClause(where)))
       .returning() as InferSelectModel<Table>[]
 
     return updatedEntity
