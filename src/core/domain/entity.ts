ea0b0784fb2a1e0fe283ee2c7754c3274f4cae44
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-08 12:25:06 -0300

    refactor: enhance Entity class to accept properties in constructor

--- src/core/domain/entity.ts
+++ src/core/domain/entity.ts
@@ -1,7 +1,11 @@
 import { uuidv7 } from 'uuidv7'
 
-export abstract class Entity {
+export abstract class Entity<Props> {
   readonly id: string = uuidv7()
   readonly createdAt = new Date()
   readonly updatedAt? = new Date()
+
+  protected constructor (data: Props) {
+    Object.assign(this, data)
+  }
 }
