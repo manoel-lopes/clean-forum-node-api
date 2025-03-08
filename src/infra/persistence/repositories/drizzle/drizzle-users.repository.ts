6c4b4c2a6b25f48a8b12002fd635c6309b6748b8
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-07 22:22:51 -0300

    refactor: simplify update method in DrizzleUsersRepository using updateOne

--- src/infra/persistence/repositories/drizzle/drizzle-users.repository.ts
+++ src/infra/persistence/repositories/drizzle/drizzle-users.repository.ts
@@ -1,10 +1,8 @@
-import { eq } from 'drizzle-orm'
 import type { User } from '@/domain/entities/user/user.entity'
 import type { UpdateUserData, UsersRepository } from '@/application/repositories/users.repository'
 import {
   BaseDrizzleRepository as BaseRepository
 } from './base/base-drizzle.repository'
-import { db } from '@/infra/persistence/drizzle/client'
 import { users } from '@/infra/persistence/drizzle/schemas/users.schema'
 
 export class DrizzleUsersRepository extends BaseRepository<typeof users> implements UsersRepository {
@@ -27,11 +25,7 @@ export class DrizzleUsersRepository extends BaseRepository<typeof users> impleme
   }
 
   async update (data: UpdateUserData): Promise<User> {
-    const { id, ...rest } = data
-    const [user] = await db.update(users)
-      .set(rest)
-      .where(eq(users.id, id))
-      .returning()
-    return user
+    const updatedUser = await this.updateOne({ where: { id: data.id } }, data)
+    return updatedUser
   }
 }
