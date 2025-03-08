4379e1dee76ae8c600e3d75578648bf99d139e86
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-08 19:55:35 -0300

    refactor: remove Optional utility from QuestionProps type definition

--- src/domain/entities/question/ports/question.props.ts
+++ src/domain/entities/question/ports/question.props.ts
@@ -1,5 +1,4 @@
 import type { OmitIdAndTimestamps } from '@/util/types/omit-id-and-timestamps'
-import type { Optional } from '@/util/types/optional'
 import type { Question } from '../question.entity'
 
-export type QuestionProps = OmitIdAndTimestamps<Optional<Omit<Question, 'slug'>, 'bestAnswerId'>>
+export type QuestionProps = OmitIdAndTimestamps<Omit<Question, 'slug'>>
