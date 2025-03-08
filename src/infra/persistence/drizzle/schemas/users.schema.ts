61c9d38167a0e3530936cc2248eaee1ab6b3dc2c
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-07 22:49:41 -0300

    refactor: remove default timestamp generation from users schema createdAt field

--- src/infra/persistence/drizzle/schemas/users.schema.ts
+++ src/infra/persistence/drizzle/schemas/users.schema.ts
@@ -1,12 +1,9 @@
 import { pgTable, text, timestamp, varchar } from 'drizzle-orm/pg-core'
-import { sql } from 'drizzle-orm'
 
 export const users = pgTable('users', {
   id: text('id').primaryKey(),
   name: varchar('name').notNull(),
   email: varchar('email').notNull().unique(),
   password: text('password').notNull(),
-  createdAt: timestamp('created_at')
-    .notNull()
-    .default(sql`CURRENT_TIMESTAMP`),
+  createdAt: timestamp('created_at').notNull(),
 })
