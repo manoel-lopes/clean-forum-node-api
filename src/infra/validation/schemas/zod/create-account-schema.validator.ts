54199452c9c9dbd39c27f803856f15c5d4981253
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-12 11:50:30 -0300

    refactor: rename BaseZodSchemaValidator import to BaseSchemaValidator in CreateAccountSchemaValidator

--- src/infra/validation/schemas/zod/create-account-schema.validator.ts
+++ src/infra/validation/schemas/zod/create-account-schema.validator.ts
@@ -2,9 +2,11 @@ import {
   createAccountRequestSchema,
 } from '@/external/zod/application/create-account/create-account-request.schema'
 import type { SchemaValidator } from '@/infra/validation/ports/schema.validator'
-import { BaseZodSchemaValidator } from './base/base-zod-schema.validator'
+import {
+  BaseZodSchemaValidator as BaseSchemaValidator
+} from './base/base-zod-schema.validator'
 
-export class CreateAccountSchemaValidator extends BaseZodSchemaValidator implements SchemaValidator {
+export class CreateAccountSchemaValidator extends BaseSchemaValidator implements SchemaValidator {
   constructor () {
     super(createAccountRequestSchema.body)
   }
