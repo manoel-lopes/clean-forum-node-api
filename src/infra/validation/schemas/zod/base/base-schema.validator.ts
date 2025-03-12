86d8d3620cab0d228304c111152a43be03c15ff6
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-12 01:31:17 -0300

    style: adjust formatting in BaseSchemaValidator class methods

--- src/infra/validation/schemas/zod/base/base-schema.validator.ts
+++ src/infra/validation/schemas/zod/base/base-schema.validator.ts
@@ -3,9 +3,9 @@ import { ZodSchemaParser } from '@/external/zod/helpers/zod-schema-parser'
 import type { SchemaValidator, SchemaParseResult } from '../../ports/schema.validator'
 
 export abstract class BaseSchemaValidator implements SchemaValidator {
-  constructor(private readonly schema: ZodSchema) {}
+  constructor (private readonly schema: ZodSchema) {}
 
-  validate(data: unknown): SchemaParseResult {
+  validate (data: unknown): SchemaParseResult {
     const parsedData = ZodSchemaParser.parse(this.schema, data)
     return Object.assign({}, ...Object.values(parsedData))
   }
