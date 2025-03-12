2edb28f1a177a4d9bada4fd8f82847e05a62ce4e
manoel-lopes <manoel.lopes.full@gmail.com>
2025-03-12 11:54:03 -0300

    style: reorder imports in ListQuestionsZodSchemaValidator

--- src/infra/validation/schemas/zod/list-questions-zod-schema.validator.ts
+++ src/infra/validation/schemas/zod/list-questions-zod-schema.validator.ts
@@ -1,10 +1,10 @@
 import {
   listQuestionsRequestSchema,
 } from '@/external/zod/application/list-questions/list-questions-request.schema'
+import type { ListItemsSchemaValidator } from '@/infra/validation/ports/list-items-schema.validator'
 import {
   BaseListItemsZodSchemaValidator as BaseSchemaValidator,
 } from './base/base-list-items-zod-schema.validator'
-import type { ListItemsSchemaValidator } from '@/infra/validation/ports/list-items-schema.validator'
 
 export class ListQuestionsZodSchemaValidator extends BaseSchemaValidator
   implements ListItemsSchemaValidator {
