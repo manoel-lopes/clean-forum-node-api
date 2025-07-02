5891c42b7c9966172011a9010182caa51382003a
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-02 17:01:40 -0300

    refactor: remove root endpoint and register account routes

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,21 +2,11 @@ import { FastifyAdapter } from '@/infra/adapters/http/http-server/fasitfy/fasitf
 import { FallbackController } from '@/infra/http/fallback/fallback.controller'
 import { ZodSchemaParser } from '@/external/zod/helpers/zod-schema-parser'
 import { env } from '@/lib/env'
-import { z } from 'zod'
+import { accountRoutes } from '../routes/account.routes'
 
 const app = new FastifyAdapter({ logger: env.NODE_ENV !== 'production' })
 app.setErrorHandler(FallbackController.handle)
 app.setValidationCompiler(ZodSchemaParser.parse)
-app.get('/', {
-  schema: {
-    response: {
-      200: z.object({
-        message: z.string()
-      })
-    }
-  }
-}, async () => {
-  return { message: 'Hello World!' }
-})
+app.register(accountRoutes)
 
 export { app }
