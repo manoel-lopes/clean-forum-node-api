af0a3cf3fe7e438ba2300633a4b4d20554dd78c9
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-02 16:32:47 -0300

    refactor(routes): refactor account and question routes to use new modular structure

--- src/main/routes/account.routes.ts
+++ src/main/routes/account.routes.ts
@@ -1,59 +1,8 @@
-import { FastifyInstance } from 'fastify'
-import { makeAuthenticateUserController } from '@main/factories/controllers/authenticate-user'
-import { makeCreateAccountController } from '@main/factories/controllers/create-account'
-import { ZodTypeProvider } from 'fastify-type-provider-zod'
-import {
-  authenticateUserSchema,
-  authenticateUserResponsesSchema
-} from '@external/zod/application/schemas/account/authenticate-user.schema'
-import {
-  createAccountSchema,
-  createAccountResponsesSchema
-} from '@external/zod/application/schemas/account/create-account.schema'
-import { errorResponseSchema } from '@external/zod/application/schemas/core/error-response.schema'
+import type { HttpServer } from '@/infra/adapters/http/ports/http-server'
+import { createAccountRoute } from './account/create-account.route'
+import { authenticateUserRoute } from './account/authenticate-user.route'
 
-export async function accountRoutes (app: FastifyInstance): Promise<void> {
-  app.withTypeProvider<ZodTypeProvider>().post(
-    '/sessions',
-    {
-      schema: {
-        tags: ['Account'],
-        summary: 'Authenticate a user',
-        body: authenticateUserSchema,
-        response: {
-          200: authenticateUserResponsesSchema,
-          401: errorResponseSchema,
-        },
-      },
-    },
-    async (request, reply) => {
-      await makeAuthenticateUserController().handle({
-        body: request.body,
-      })
-
-      return reply.status(200).send()
-    }
-  )
-
-  app.withTypeProvider<ZodTypeProvider>().post(
-    '/users',
-    {
-      schema: {
-        tags: ['Account'],
-        summary: 'Create an account',
-        body: createAccountSchema,
-        response: {
-          201: createAccountResponsesSchema,
-          409: errorResponseSchema,
-        },
-      },
-    },
-    async (request, reply) => {
-      await makeCreateAccountController().handle({
-        body: request.body,
-      })
-
-      return reply.status(201).send()
-    }
-  )
+export function accountRoutes (app: HttpServer) {
+  app.register(createAccountRoute)
+  app.register(authenticateUserRoute)
 }
--- src/main/routes/question.routes.ts
+++ src/main/routes/question.routes.ts
@@ -1,92 +1,10 @@
-import { FastifyInstance } from 'fastify'
-import { makeCreateQuestionController } from '@main/factories/controllers/create-question'
-import { makeAnswerQuestionController } from '@main/factories/controllers/answer-question'
-import {
-  makeChooseQuestionBestAnswerController
-} from '@main/factories/controllers/choose-question-best-answer'
-import { ZodTypeProvider } from 'fastify-type-provider-zod'
-import {
-  createQuestionSchema,
-  createQuestionResponsesSchema
-} from '@external/zod/application/schemas/question/create-question.schema'
-import { errorResponseSchema } from '@external/zod/application/schemas/core/error-response.schema'
-import {
-  answerQuestionSchema,
-  answerQuestionResponsesSchema
-} from '@external/zod/application/schemas/question/answer-question.schema'
-import {
-  chooseQuestionBestAnswerSchema,
-  chooseQuestionBestAnswerResponsesSchema
-} from '@external/zod/application/schemas/question/choose-question-best-answer.schema'
+import type { HttpServer } from '@/infra/adapters/http/ports/http-server'
+import { createQuestionRoute } from './question/create-question.route'
+import { answerQuestionRoute } from './question/answer-question.route'
+import { chooseQuestionBestAnswerRoute } from './question/choose-question-best-answer.route'
 
-export async function questionRoutes (app: FastifyInstance): Promise<void> {
-  app.withTypeProvider<ZodTypeProvider>().post(
-    '/questions',
-    {
-      schema: {
-        tags: ['Question'],
-        summary: 'Create a question',
-        body: createQuestionSchema,
-        response: {
-          201: createQuestionResponsesSchema,
-          409: errorResponseSchema,
-        },
-      },
-    },
-    async (request, reply) => {
-      await makeCreateQuestionController().handle({
-        body: request.body,
-        user: request.user,
-      })
-
-      return reply.status(201).send()
-    }
-  )
-
-  app.withTypeProvider<ZodTypeProvider>().post(
-    '/questions/:questionId/answers',
-    {
-      schema: {
-        tags: ['Question'],
-        summary: 'Answer a question',
-        body: answerQuestionSchema,
-        response: {
-          201: answerQuestionResponsesSchema,
-        },
-      },
-    },
-    async (request, reply) => {
-      await makeAnswerQuestionController().handle({
-        body: request.body,
-        params: request.params,
-        user: request.user,
-      })
-
-      return reply.status(201).send()
-    }
-  )
-
-  app.withTypeProvider<ZodTypeProvider>().patch(
-    '/answers/:answerId/best',
-    {
-      schema: {
-        tags: ['Question'],
-        summary: 'Choose question best answer',
-        body: chooseQuestionBestAnswerSchema,
-        response: {
-          204: chooseQuestionBestAnswerResponsesSchema,
-          403: errorResponseSchema,
-          404: errorResponseSchema,
-        },
-      },
-    },
-    async (request, reply) => {
-      await makeChooseQuestionBestAnswerController().handle({
-        params: request.params,
-        user: request.user,
-      })
-
-      return reply.status(204).send()
-    }
-  )
+export async function questionRoutes (app: HttpServer) {
+  createQuestionRoute(app)
+  answerQuestionRoute(app)
+  chooseQuestionBestAnswerRoute(app)
 }
