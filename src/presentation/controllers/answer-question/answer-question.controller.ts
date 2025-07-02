116878b01f0cb75e26ee6b150cb5050a91483f0a
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-02 14:56:37 -0300

    feat(controller): implement answer question controller\n\nimplement the , responsible for handling HTTP requests for answering questions.

---
+++ src/presentation/controllers/answer-question/answer-question.controller.ts
@@ -0,0 +1,24 @@
+import { WebController } from '@core/presentation/web-controller'
+import { AnswerQuestionUseCase } from '@application/usecases/answer-question/answer-question.usecase'
+import { HttpRequest, HttpResponse } from '@infra/adapters/http/ports/http-protocol'
+import { created } from '@presentation/helpers/http-helpers'
+
+export class AnswerQuestionController implements WebController {
+  constructor(
+    private answerQuestionUseCase: AnswerQuestionUseCase,
+  ) {}
+
+  public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const { content } = httpRequest.body
+    const { questionId } = httpRequest.params
+    const { authorId } = httpRequest.user
+
+    const { answer } = await this.answerQuestionUseCase.execute({
+      content,
+      authorId,
+      questionId,
+    })
+
+    return created(answer)
+  }
+}
