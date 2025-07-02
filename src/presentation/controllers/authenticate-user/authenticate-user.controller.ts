a8a6f412db76253ce4d51cc66186db691c7ca5db
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-02 14:55:55 -0300

    feat(controller): implement authenticate user controller\n\nimplement the , responsible for handling HTTP requests for user authentication.

---
+++ src/presentation/controllers/authenticate-user/authenticate-user.controller.ts
@@ -0,0 +1,30 @@
+import { WebController } from '@core/presentation/web-controller'
+import { AuthenticateUserUseCase } from '@application/usecases/authenticate-user/authenticate-user.usecase'
+import { HttpRequest, HttpResponse } from '@infra/adapters/http/ports/http-protocol'
+import { InvalidPasswordError } from '@application/usecases/authenticate-user/errors/invalid-password.error'
+import { badRequest, ok, unauthorized } from '@presentation/helpers/http-helpers'
+
+export class AuthenticateUserController implements WebController {
+  constructor(
+    private authenticateUserUseCase: AuthenticateUserUseCase,
+  ) {}
+
+  public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { email, password } = httpRequest.body
+
+      const { accessToken } = await this.authenticateUserUseCase.execute({
+        email,
+        password,
+      })
+
+      return ok({ accessToken })
+    } catch (error) {
+      if (error instanceof InvalidPasswordError) {
+        return unauthorized(error)
+      }
+
+      throw error
+    }
+  }
+}
