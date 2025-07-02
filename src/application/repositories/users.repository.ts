db21825ef852815534da7711bb6312ccc9fa1caf
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-02 14:47:32 -0300

    feat(repository): add find by id and delete methods to users repository\n\nadd  and  methods to the  interface.

--- src/application/repositories/users.repository.ts
+++ src/application/repositories/users.repository.ts
@@ -3,4 +3,6 @@ import { User } from '@domain/entities/user/user.entity'
 export interface UsersRepository {
   create(user: User): Promise<void>
   findByEmail(email: string): Promise<User | null>
+  findById(id: string): Promise<User | null>
+  delete(id: string): Promise<void>
 }
