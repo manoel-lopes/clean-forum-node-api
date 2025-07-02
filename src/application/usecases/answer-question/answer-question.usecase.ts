24f0677638ec52d41811e8c4d03be19125252892
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-02 16:17:58 -0300

    refactor(answer-question): update AnswerQuestionUseCase

--- src/application/usecases/answer-question/answer-question.usecase.ts
+++ src/application/usecases/answer-question/answer-question.usecase.ts
@@ -1,33 +1,29 @@
-import { UseCase } from '@core/application/use-case'
-import { Answer } from '@domain/entities/answer/answer.entity'
-import { AnswersRepository } from '@application/repositories/answers.repository'
+import type { UseCase } from '@/core/application/use-case'
+import type { AnswersRepository } from '@/application/repositories/answers.repository'
+import type { UsersRepository } from '@/application/repositories/users.repository'
+import { Answer } from '@/domain/entities/answer/answer.entity'
+import type { AnswerProps } from '@/domain/entities/answer/ports/answer.props'
+import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
 
-interface AnswerQuestionRequest {
-  content: string
-  authorId: string
-  questionId: string
-}
+export type AnswerQuestionRequest = AnswerProps
 
-interface AnswerQuestionResponse {
-  answer: Answer
-}
+export class AnswerQuestionUseCase implements UseCase {
+  constructor (
+    private readonly answersRepository: AnswersRepository,
+    private readonly userRepository: UsersRepository
+  ) {
+    Object.freeze(this)
+  }
 
-export class AnswerQuestionUseCase implements UseCase<AnswerQuestionRequest, AnswerQuestionResponse> {
-  constructor(
-    private answersRepository: AnswersRepository,
-  ) {}
-
-  public async execute({ content, authorId, questionId }: AnswerQuestionRequest): Promise<AnswerQuestionResponse> {
-    const answer = Answer.create({
-      content,
-      authorId,
-      questionId,
-    })
-
-    await this.answersRepository.create(answer)
-
-    return {
-      answer,
+  async execute (req: AnswerQuestionRequest): Promise<Answer> {
+    const { content, authorId, questionId } = req
+    const author = await this.userRepository.findById(authorId)
+    if (!author) {
+      throw new ResourceNotFoundError('User')
     }
+
+    const answer = Answer.create({ content, authorId, questionId })
+    await this.answersRepository.save(answer)
+    return answer
   }
 }
