3aea9d58d6d747ab68a7825ed84ab1750b8f1d6e
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-02 16:18:49 -0300

    refactor(password-hasher): update PasswordHasher and PasswordHasherStub

--- src/infra/adapters/crypto/stubs/password-hasher.stub.ts
+++ src/infra/adapters/crypto/stubs/password-hasher.stub.ts
@@ -1,11 +1,11 @@
 import { PasswordHasher } from '../ports/password-hasher'
 
 export class PasswordHasherStub implements PasswordHasher {
-  public async hash(password: string): Promise<string> {
+  public async hash (password: string): Promise<string> {
     return `${password}-hashed`
   }
 
-  public async compare(password: string, hash: string): Promise<boolean> {
+  public async compare (password: string, hash: string): Promise<boolean> {
     return `${password}-hashed` === hash
   }
 }
