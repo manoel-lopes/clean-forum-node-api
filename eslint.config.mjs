7ec54f815a92149e34e07452ae8898dfad88b64e
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-02 15:08:17 -0300

    style: enforce stricter eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import neostandard, { resolveIgnoresFromGitignore } from 'neostandard'
 import vitest from '@vitest/eslint-plugin'
+import unusedImports from 'eslint-plugin-unused-imports'
+import importNewlines from 'eslint-plugin-import-newlines'
 
 export default [
   eslint.configs.recommended,
@@ -12,6 +14,8 @@ export default [
   {
     plugins: {
       vitest,
+      'import-newlines': importNewlines,
+      'unused-imports': unusedImports,
     },
     languageOptions: {
       globals: {
@@ -19,38 +23,34 @@ export default [
       },
     },
     rules: {
-      '@stylistic/object-curly-spacing': 'off',
-      '@stylistic/object-curly-newline': 'off',
-      '@stylistic/object-property-newline': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'import-newlines/enforce': ['error', { items: 1000, 'max-len': 102 }],
       'no-useless-constructor': 'off',
       '@typescript-eslint/no-useless-constructor': 'off',
       'no-unused-vars': 'off',
       'no-var': 'error',
       'no-console': ['error', { allow: ['error'] }],
-      '@stylistic/function-paren-newline': ['error', 'multiline'],
-      '@stylistic/max-len': ['warn', {
-        code: 102,
-        tabWidth: 2,
-        ignoreUrls: true,
-        ignoreComments: false,
-      }],
-      '@stylistic/space-before-function-paren': ['error', {
-        anonymous: 'always',
-        asyncArrow: 'always',
-        named: 'always',
-      }],
+      '@stylistic/function-paren-newline': ['warn', 'multiline'],
+      '@stylistic/space-before-function-paren': [
+        'warn',
+        { anonymous: 'always', asyncArrow: 'always', named: 'always' },
+      ],
       '@stylistic/multiline-ternary': 'off',
       '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/parameter-properties': ['error', {
-        allow: [
-          'readonly',
-          'private',
-          'private readonly'],
-      }],
-      '@typescript-eslint/no-unused-vars': ['error', {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-      }],
+      '@typescript-eslint/parameter-properties': [
+        'error',
+        { allow: ['readonly', 'private', 'private readonly'] },
+      ],
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
     },
   },
 ]
