7235b922b9c887f22de5882c5cf20c3c364a3d87
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-02 17:06:08 -0300

    feat: enhance FastifyAdapter configuration with OpenAPI settings

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -5,7 +5,15 @@ import { env } from '@/lib/env'
 import { accountRoutes } from '../routes/account.routes'
 import { questionRoutes } from '../routes/question.routes'
 
-const app = new FastifyAdapter({ logger: env.NODE_ENV !== 'production' })
+const app = new FastifyAdapter({
+  logger: env.NODE_ENV !== 'production',
+  openapi: {
+    info: {
+      title: 'Clean Forum Node API',
+      version: '1.0.0',
+    },
+  }
+})
 app.setErrorHandler(FallbackController.handle)
 app.setValidationCompiler(ZodSchemaParser.parse)
 app.register(accountRoutes)
