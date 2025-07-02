16899089a6c68e6ad215f0ee01f1cf22e62d8fd4
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-02 17:02:08 -0300

    feat: register question routes in the Fastify application

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -3,10 +3,12 @@ import { FallbackController } from '@/infra/http/fallback/fallback.controller'
 import { ZodSchemaParser } from '@/external/zod/helpers/zod-schema-parser'
 import { env } from '@/lib/env'
 import { accountRoutes } from '../routes/account.routes'
+import { questionRoutes } from '../routes/question.routes'
 
 const app = new FastifyAdapter({ logger: env.NODE_ENV !== 'production' })
 app.setErrorHandler(FallbackController.handle)
 app.setValidationCompiler(ZodSchemaParser.parse)
 app.register(accountRoutes)
+app.register(questionRoutes)
 
 export { app }
