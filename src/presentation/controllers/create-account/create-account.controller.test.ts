7c0114d2f7aad5259276f63c22d58db5c3fae05f
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-02 14:55:35 -0300

    feat(controller): implement create account controller\n\nimplement the , responsible for handling HTTP requests for user account creation.

---
+++ src/presentation/controllers/create-account/create-account.controller.test.ts
@@ -0,0 +1,66 @@
+import { CreateAccountController } from './create-account.controller'
+import { CreateAccountUseCase } from '@application/usecases/create-account/create-account.usecase'
+import { HttpRequest } from '@infra/adapters/http/ports/http-protocol'
+import { UserWithEmailAlreadyRegisteredError } from '@application/usecases/create-account/errors/user-with-email-already-registered.error'
+import { makeUser } from '@test/util/factories/domain/make-user'
+
+describe('CreateAccountController', () => {
+  let createAccountUseCase: CreateAccountUseCase
+  let sut: CreateAccountController
+
+  beforeEach(() => {
+    createAccountUseCase = { execute: jest.fn() } as unknown as CreateAccountUseCase
+    sut = new CreateAccountController(createAccountUseCase)
+  })
+
+  it('should return 201 if valid data is provided', async () => {
+    const user = makeUser()
+    jest.spyOn(createAccountUseCase, 'execute').mockResolvedValueOnce({ user })
+
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        password: 'password',
+      },
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(201)
+    expect(httpResponse.body).toEqual(user)
+  })
+
+  it('should return 409 if user with email already registered error occurs', async () => {
+    jest.spyOn(createAccountUseCase, 'execute').mockRejectedValueOnce(new UserWithEmailAlreadyRegisteredError())
+
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        password: 'password',
+      },
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(409)
+    expect(httpResponse.body).toBeInstanceOf(UserWithEmailAlreadyRegisteredError)
+  })
+
+  it('should return 500 if any other error occurs', async () => {
+    jest.spyOn(createAccountUseCase, 'execute').mockRejectedValueOnce(new Error())
+
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        password: 'password',
+      },
+    }
+
+    const promise = sut.handle(httpRequest)
+
+    await expect(promise).rejects.toThrow(Error)
+  })
+})
---
+++ src/presentation/controllers/create-account/create-account.controller.ts
@@ -0,0 +1,31 @@
+import { WebController } from '@core/presentation/web-controller'
+import { CreateAccountUseCase } from '@application/usecases/create-account/create-account.usecase'
+import { HttpRequest, HttpResponse } from '@infra/adapters/http/ports/http-protocol'
+import { UserWithEmailAlreadyRegisteredError } from '@application/usecases/create-account/errors/user-with-email-already-registered.error'
+import { badRequest, conflict, created } from '@presentation/helpers/http-helpers'
+
+export class CreateAccountController implements WebController {
+  constructor(
+    private createAccountUseCase: CreateAccountUseCase,
+  ) {}
+
+  public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { name, email, password } = httpRequest.body
+
+      const { user } = await this.createAccountUseCase.execute({
+        name,
+        email,
+        password,
+      })
+
+      return created(user)
+    } catch (error) {
+      if (error instanceof UserWithEmailAlreadyRegisteredError) {
+        return conflict(error)
+      }
+
+      throw error
+    }
+  }
+}
