7c8ec2ba1f1e4c36c52179d4224f1b2ae8e646a8
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-06 13:33:04 -0300

    feat(users): add get-user-by-email route implementation

---
+++ src/main/fastify/routes/users/get-user-by-email/get-user-by-email.route.ts
@@ -0,0 +1,20 @@
+import type { FastifyInstance } from 'fastify'
+
+import { makeGetUserByEmailController } from '@/main/factories/get-user-by-email'
+import { getUserByEmailSchema } from '@/main/zod/schemas/presentation/users/get-user-by-email.schema'
+import { getUserByEmailResponsesSchema } from '@/main/zod/schemas/presentation/users/get-user-by-email-responses.schema'
+
+import { adaptRoute } from '@/util/adapt-route'
+
+export async function getUserByEmailRoute (app: FastifyInstance, tags: string[]) {
+  app.get('/users/:email', {
+    schema: {
+      tags,
+      description: 'Get user by email',
+      params: getUserByEmailSchema,
+      response: getUserByEmailResponsesSchema
+    }
+  },
+  adaptRoute(makeGetUserByEmailController())
+  )
+}
