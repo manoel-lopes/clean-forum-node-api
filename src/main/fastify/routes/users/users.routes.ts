805b56d95fbd6533e7f9c81f2f6b91e21aaba5ce
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-06 13:32:23 -0300

    feat(users): add get-user-by-email route

--- src/main/fastify/routes/users/users.routes.ts
+++ src/main/fastify/routes/users/users.routes.ts
@@ -4,7 +4,8 @@ import { registerRoutes } from '@/main/fastify/helpers/register-routes'
 
 import { authenticateUserRoute } from './authenticate-user/authenticate-user.route'
 import { createAccountRoute } from './create-account/create-account.route'
+import { getUserByEmailRoute } from './get-user-by-email/get-user-by-email.route'
 
 export async function usersRoutes (app: FastifyInstance) {
-  registerRoutes(app, [createAccountRoute, authenticateUserRoute])
+  registerRoutes(app, [createAccountRoute, authenticateUserRoute, getUserByEmailRoute])
 }
