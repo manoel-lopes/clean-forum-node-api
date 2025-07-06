630ec35a5e331e96963ff974950b44115d1b42f9
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-06 15:53:19 -0300

    test: add answer question controller tests

---
+++ src/presentation/controllers/answer-question/answer-question.controller.test.ts
@@ -0,0 +1,50 @@
+import type { UseCase } from '@/core/application/use-case'
+
+import { UseCaseStub } from '@/infra/doubles/stubs/use-case.stub'
+
+import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
+
+import { created, notFound } from '@/presentation/helpers/http-helpers'
+
+import { AnswerQuestionController } from './answer-question.controller'
+
+describe('AnswerQuestionController', () => {
+  let sut: AnswerQuestionController
+  let answerQuestionUseCase: UseCase
+
+  beforeEach(() => {
+    answerQuestionUseCase = new UseCaseStub()
+    sut = new AnswerQuestionController(answerQuestionUseCase)
+  })
+
+  const httpRequest = {
+    body: {
+      questionId: 'any_question_id',
+      content: 'any_content',
+      authorId: 'any_author_id'
+    }
+  }
+
+  it('should return a not found error response if the author is not found', async () => {
+    const error = new ResourceNotFoundError('User')
+    vi.spyOn(answerQuestionUseCase, 'execute').mockRejectedValue(error)
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual(notFound(error))
+  })
+
+  it('should return an unknown error response if an unexpect error occur', async () => {
+    const error = new Error('any_error')
+
+    vi.spyOn(answerQuestionUseCase, 'execute').mockRejectedValue(error)
+
+    await expect(sut.handle(httpRequest)).rejects.toThrow(error)
+  })
+
+  it('should return a created response on the creation of an answer', async () => {
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual(created())
+  })
+})
