04516098d4134c9770db6fbcbf9a1b6e31c43f3f
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-06 13:50:17 -0300

    fix: create account responses schema to use z.null()

--- src/main/zod/schemas/presentation/users/create-account-responses.schema.ts
+++ src/main/zod/schemas/presentation/users/create-account-responses.schema.ts
@@ -1,7 +1,9 @@
+import { z } from 'zod'
+
 import { errorResponseSchema } from '../../core/error-response.schema'
 
 export const createAccountResponsesSchema = {
-  201: null,
+  201: z.null(),
   400: errorResponseSchema,
   409: errorResponseSchema
 }
