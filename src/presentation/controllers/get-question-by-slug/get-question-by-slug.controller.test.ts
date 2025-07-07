c0f5e7a509393d80a739f5ff35579990e07e7acc
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-07 12:34:27 -0300

    feat: add: get question by slug controller

---
+++ src/presentation/controllers/get-question-by-slug/get-question-by-slug.controller.test.ts
@@ -0,0 +1,69 @@
+import type { UseCase } from '@/core/application/use-case'
+
+import { UseCaseStub } from '@/infra/doubles/stubs/use-case.stub'
+
+import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
+
+import { GetQuestionBySlugController } from './get-question-by-slug.controller'
+
+describe('GetQuestionBySlugController', () => {
+  let sut: GetQuestionBySlugController
+  let getQuestionBySlugUseCase: UseCase
+
+  beforeEach(() => {
+    getQuestionBySlugUseCase = new UseCaseStub()
+    sut = new GetQuestionBySlugController(getQuestionBySlugUseCase)
+  })
+
+  const httpRequest = {
+    params: {
+      slug: 'any_slug'
+    }
+  }
+
+  it('should return 404 and a not found error response if the question is not found', async () => {
+    vi.spyOn(getQuestionBySlugUseCase, 'execute').mockRejectedValue(
+      new ResourceNotFoundError('Question')
+    )
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(404)
+    expect(httpResponse.body).toEqual({
+      error: 'Not Found',
+      message: 'Question not found',
+    })
+  })
+
+  it('should throw an unknown error response if an unexpect error occur', async () => {
+    const error = new Error('any_error')
+
+    vi.spyOn(getQuestionBySlugUseCase, 'execute').mockRejectedValue(error)
+
+    await expect(sut.handle(httpRequest)).rejects.toThrow(error)
+  })
+
+  it('should return 200 and an ok response with the question data on successful retrieval', async () => {
+    const question = {
+      id: 'any_id',
+      title: 'any_title',
+      slug: 'any_slug',
+      content: 'any_content',
+      authorId: 'any_author_id',
+      createdAt: new Date().toISOString()
+    }
+    vi.spyOn(getQuestionBySlugUseCase, 'execute').mockResolvedValue(question)
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: question.id,
+      title: question.title,
+      slug: question.slug,
+      content: question.content,
+      authorId: question.authorId,
+      createdAt: question.createdAt
+    })
+  })
+})
---
+++ src/presentation/controllers/get-question-by-slug/get-question-by-slug.controller.ts
@@ -0,0 +1,25 @@
+import type { WebController } from '@/core/presentation/web-controller'
+import type { UseCase } from '@/core/application/use-case'
+
+import type { HttpRequest, HttpResponse } from '@/infra/http/ports/http-protocol'
+
+import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
+
+import { notFound, ok } from '@/presentation/helpers/http-helpers'
+
+export class GetQuestionBySlugController implements WebController {
+  constructor (private readonly getQuestionBySlugUseCase: UseCase) {}
+
+  async handle (req: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { slug } = req.params
+      const question = await this.getQuestionBySlugUseCase.execute({ slug })
+      return ok(question)
+    } catch (error) {
+      if (error instanceof ResourceNotFoundError) {
+        return notFound(error)
+      }
+      throw error
+    }
+  }
+}
