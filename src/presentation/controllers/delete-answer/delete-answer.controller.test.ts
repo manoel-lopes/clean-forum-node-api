f194733a0fddba3c912bdcc0624c69796d4cf32d
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-07 12:33:21 -0300

    feat: add delete answer controller

---
+++ src/presentation/controllers/delete-answer/delete-answer.controller.test.ts
@@ -0,0 +1,68 @@
+import type { UseCase } from '@/core/application/use-case'
+
+import { UseCaseStub } from '@/infra/doubles/stubs/use-case.stub'
+
+import { NotAuthorError } from '@/application/errors/not-author.error'
+import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
+
+import { DeleteAnswerController } from './delete-answer.controller'
+
+describe('DeleteAnswerController', () => {
+  let sut: DeleteAnswerController
+  let deleteAnswerUseCase: UseCase
+
+  beforeEach(() => {
+    deleteAnswerUseCase = new UseCaseStub()
+    sut = new DeleteAnswerController(deleteAnswerUseCase)
+  })
+
+  const httpRequest = {
+    params: {
+      id: 'any_id'
+    },
+    userId: 'any_user_id'
+  }
+
+  it('should return 404 and a not found error response if the answer is not found', async () => {
+    vi.spyOn(deleteAnswerUseCase, 'execute').mockRejectedValue(
+      new ResourceNotFoundError('Answer')
+    )
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(404)
+    expect(httpResponse.body).toEqual({
+      error: 'Not Found',
+      message: 'Answer not found',
+    })
+  })
+
+  it('should return 403 and a forbidden error response if the user is not the author', async () => {
+    vi.spyOn(deleteAnswerUseCase, 'execute').mockRejectedValue(
+      new NotAuthorError('answer')
+    )
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(403)
+    expect(httpResponse.body).toEqual({
+      error: 'Forbidden',
+      message: 'The user is not the author of the answer',
+    })
+  })
+
+  it('should throw an unknown error response if an unexpect error occur', async () => {
+    const error = new Error('any_error')
+
+    vi.spyOn(deleteAnswerUseCase, 'execute').mockRejectedValue(error)
+
+    await expect(sut.handle(httpRequest)).rejects.toThrow(error)
+  })
+
+  it('should return 204 on successful answer deletion', async () => {
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(204)
+    expect(httpResponse.body).toBeNull()
+  })
+})
---
+++ src/presentation/controllers/delete-answer/delete-answer.controller.ts
@@ -0,0 +1,30 @@
+import type { WebController } from '@/core/presentation/web-controller'
+import type { UseCase } from '@/core/application/use-case'
+
+import type { HttpRequest, HttpResponse } from '@/infra/http/ports/http-protocol'
+
+import { NotAuthorError } from '@/application/errors/not-author.error'
+import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
+
+import { forbidden, noContent, notFound } from '@/presentation/helpers/http-helpers'
+
+export class DeleteAnswerController implements WebController {
+  constructor (private readonly deleteAnswerUseCase: UseCase) {}
+
+  async handle (req: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { id } = req.params
+      const { userId } = req
+      await this.deleteAnswerUseCase.execute({ answerId: id, authorId: userId })
+      return noContent()
+    } catch (error) {
+      if (error instanceof ResourceNotFoundError) {
+        return notFound(error)
+      }
+      if (error instanceof NotAuthorError) {
+        return forbidden(error)
+      }
+      throw error
+    }
+  }
+}
