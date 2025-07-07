8035eb2a4952ad67372ae0f940171230de0eed68
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-06 22:25:45 -0300

    refactor(error): remove custom error names property setting

--- src/application/errors/not-author.error.ts
+++ src/application/errors/not-author.error.ts
@@ -3,6 +3,5 @@ type Resource = 'question' | 'answer'
 export class NotAuthorError extends Error {
   constructor (resource: Resource) {
     super(`The user is not the author of the ${resource}`)
-    this.name = 'NotAuthorError'
   }
 }
--- src/application/usecases/authenticate-user/errors/invalid-password.error.ts
+++ src/application/usecases/authenticate-user/errors/invalid-password.error.ts
@@ -1,6 +1,5 @@
 export class InvalidPasswordError extends Error {
   constructor () {
     super('Invalid password')
-    this.name = 'InvalidPasswordError'
   }
 }
