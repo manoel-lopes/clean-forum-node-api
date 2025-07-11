c415b788dd2b555733b4572abbec70e95cc1bc06
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-11 15:33:25 -0300

    fix(schema): update question schema to use nullable for bestAnswerId and datetime for createdAt and updatedAt

--- src/infra/validation/zod/schemas/domain/question.schema.ts
+++ src/infra/validation/zod/schemas/domain/question.schema.ts
@@ -1,6 +1,4 @@
-import z from 'zod'
-
-import type { Question } from '@/domain/entities/question/question.entity'
+import { z } from 'zod'
 
 export const questionSchema = z.object({
   id: z.string().uuid(),
@@ -8,7 +6,7 @@ export const questionSchema = z.object({
   content: z.string(),
   slug: z.string(),
   authorId: z.string().uuid(),
-  bestAnswerId: z.string().uuid().optional(),
-  createdAt: z.date(),
-  updatedAt: z.date()
-}) satisfies z.ZodType<Question>
+  bestAnswerId: z.string().uuid().nullable(),
+  createdAt: z.string().datetime(),
+  updatedAt: z.string().datetime().nullable()
+})
