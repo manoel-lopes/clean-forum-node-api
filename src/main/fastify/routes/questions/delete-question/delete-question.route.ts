536f0befcb3ee6638b3d75d8270d234bc489b8fe
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 16:46:43 -0300

    style(questions): remove empty lines from fastify question routes

--- src/main/fastify/routes/questions/delete-question/delete-question.route.ts
+++ src/main/fastify/routes/questions/delete-question/delete-question.route.ts
@@ -1,13 +1,10 @@
 import type { FastifyInstance } from 'fastify'
-
 import {
   deleteQuestionBodySchema,
   deleteQuestionParamsSchema,
   deleteQuestionResponsesSchema
 } from '@/infra/validation/zod/schemas/presentation/questions/delete-question.schemas'
-
 import { makeDeleteQuestionController } from '@/main/factories/delete-question'
-
 import { adaptRoute } from '@/util/adapt-route'
 
 export async function deleteQuestionRoute (app: FastifyInstance, tags: string[]) {
