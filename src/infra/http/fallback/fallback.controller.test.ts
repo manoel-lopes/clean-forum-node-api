516f90224abda8a60c933bb303e075f94d087427
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 16:41:06 -0300

    style(http): remove empty lines from http related files

--- src/infra/http/fallback/fallback.controller.test.ts
+++ src/infra/http/fallback/fallback.controller.test.ts
@@ -1,5 +1,4 @@
 import { SchemaValidationError } from '@/infra/validation/errors/schema-validation.error'
-
 import { FallbackController } from './fallback.controller'
 
 const mockResponse = () => {
@@ -10,7 +9,6 @@ const mockResponse = () => {
   }
   return res
 }
-
 describe('FallbackController', () => {
   let res: ReturnType<typeof mockResponse>
   const mocks = vi.hoisted(() => ({
@@ -18,68 +16,51 @@ describe('FallbackController', () => {
       NODE_ENV: 'development',
     }
   }))
-
   vi.mock('@/lib/env', () => mocks)
-
   beforeEach(() => {
     res = mockResponse()
     vi.spyOn(console, 'error').mockClear()
   })
-
   it('should handle a schema validation error sending a bad request error http response', () => {
     const error = new SchemaValidationError('Field is required')
-
     FallbackController.handle(error, {}, res)
-
     expect(res.code).toHaveBeenCalledWith(400)
     expect(res.send).toHaveBeenCalledWith({
       error: 'Bad Request',
       message: error.message,
     })
   })
-
   it('should handle an schema validation error with unprocessable entity error http response', () => {
     const error = new SchemaValidationError('Invalid format')
-
     FallbackController.handle(error, {}, res)
-
     expect(res.code).toHaveBeenCalledWith(422)
     expect(res.send).toHaveBeenCalledWith({
       error: 'Unprocessable Entity',
       message: error.message,
     })
   })
-
   it('should handle an empty request body error sending a bad request error http response', () => {
     const error = new SchemaValidationError('Request body is missing or empty')
-
     FallbackController.handle(error, {}, res)
-
     expect(res.code).toHaveBeenCalledWith(400)
     expect(res.send).toHaveBeenCalledWith({
       error: 'Bad Request',
       message: error.message,
     })
   })
-
   it('should log an unexpected error and return an internal server error http response', () => {
     const error = new Error('Unexpected error')
     vi.spyOn(console, 'error').mockImplementation(() => {})
-
     FallbackController.handle(error, {}, res)
-
     expect(console.error).toHaveBeenCalledWith({ errors: error.message })
     expect(res.code).toHaveBeenCalledWith(500)
     expect(res.send).toHaveBeenCalledWith({ error: 'Internal Server Error' })
   })
-
   it('should not log an unexpected error and return an internal server error http response', () => {
     mocks.env.NODE_ENV = 'production'
     const error = new Error('Unexpected error')
     vi.spyOn(console, 'error').mockImplementation(() => {})
-
     FallbackController.handle(error, {}, res)
-
     expect(console.error).not.toHaveBeenCalled()
     expect(res.code).toHaveBeenCalledWith(500)
     expect(res.send).toHaveBeenCalledWith({ error: 'Internal Server Error' })
