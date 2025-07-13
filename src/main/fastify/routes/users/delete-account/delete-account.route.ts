de486fff7a6b8197075737a9cc1c09b870cafe36
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 16:47:33 -0300

    style(users): remove empty lines from fastify user routes

--- src/main/fastify/routes/users/delete-account/delete-account.route.ts
+++ src/main/fastify/routes/users/delete-account/delete-account.route.ts
@@ -1,12 +1,9 @@
 import type { FastifyInstance } from 'fastify'
-
 import {
   deleteAccountParamsSchema,
   deleteAccountResponsesSchema
 } from '@/infra/validation/zod/schemas/presentation/users/delete-account.schemas'
-
 import { makeDeleteAccountController } from '@/main/factories/delete-account'
-
 import { adaptRoute } from '@/util/adapt-route'
 
 export async function deleteAccountRoute (app: FastifyInstance, tags: string[]) {
--- src/main/fastify/routes/users/fetch-users/fetch-users.route.ts
+++ src/main/fastify/routes/users/fetch-users/fetch-users.route.ts
@@ -1,10 +1,7 @@
 import type { FastifyInstance } from 'fastify'
-
 import { paginationParamsSchema } from '@/infra/validation/zod/schemas/core/pagination-params.schema'
 import { fetchUsersResponsesSchemas } from '@/infra/validation/zod/schemas/presentation/users/fetch-users.schemas'
-
 import { makeFetchUsersController } from '@/main/factories/fetch-users'
-
 import { adaptRoute } from '@/util/adapt-route'
 
 export async function fetchUsersRoute (app: FastifyInstance, tags: string[]) {
