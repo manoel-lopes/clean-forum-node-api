9c14eca16f7f253a40b23fc4d75c7a20540f4ed4
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 16:36:12 -0300

    style: remove empty line from error files

--- src/application/errors/resource-not-found.error.ts
+++ src/application/errors/resource-not-found.error.ts
@@ -1,5 +1,4 @@
 type Resource = 'User' | 'Answer' | 'Question' | 'Comment'
-
 export class ResourceNotFoundError extends Error {
   constructor (resource: Resource) {
     super(`${resource} not found`)
