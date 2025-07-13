21cdd9c0e4c89d740d4414bbddfe670d66ac90ce
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 14:08:18 -0300

    refactor(controllers): add HttpResponse type to handle method This commit adds the HttpResponse type to the handle method in the FetchAnswersController and FetchQuestionsController for better type safety.

--- src/presentation/controllers/fetch-answers/fetch-answers.controller.ts
+++ src/presentation/controllers/fetch-answers/fetch-answers.controller.ts
@@ -1,6 +1,6 @@
 import type { WebController } from '@/core/presentation/web-controller'
 
-import type { HttpRequest } from '@/infra/http/ports/http-protocol'
+import type { HttpRequest, HttpResponse } from '@/infra/http/ports/http-protocol'
 
 import type { AnswersRepository } from '@/application/repositories/answers.repository'
 
@@ -9,7 +9,7 @@ import { ok } from '@/presentation/helpers/http-helpers'
 export class FetchAnswersController implements WebController {
   constructor (private readonly answersRepository: AnswersRepository) {}
 
-  async handle (req: HttpRequest) {
+  async handle (req: HttpRequest): Promise<HttpResponse> {
     const page = req.query?.page ?? 1
     const pageSize = req.query?.pageSize ?? 20
     const answers = await this.answersRepository.findMany({ page, pageSize })
--- src/presentation/controllers/fetch-questions/fetch-questions.controller.ts
+++ src/presentation/controllers/fetch-questions/fetch-questions.controller.ts
@@ -1,6 +1,6 @@
 import type { WebController } from '@/core/presentation/web-controller'
 
-import type { HttpRequest } from '@/infra/http/ports/http-protocol'
+import type { HttpRequest, HttpResponse } from '@/infra/http/ports/http-protocol'
 
 import type { QuestionsRepository } from '@/application/repositories/questions.repository'
 
@@ -9,7 +9,7 @@ import { ok } from '@/presentation/helpers/http-helpers'
 export class FetchQuestionsController implements WebController {
   constructor (private readonly questionsRepository: QuestionsRepository) {}
 
-  async handle (req: HttpRequest) {
+  async handle (req: HttpRequest): Promise<HttpResponse> {
     const page = req.query?.page ?? 1
     const pageSize = req.query?.pageSize ?? 20
     const questions = await this.questionsRepository.findMany({ page, pageSize })
