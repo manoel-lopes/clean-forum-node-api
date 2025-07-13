833e0673da93bd3edfdf80568f404d20b12a8b33
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 16:46:19 -0300

    refactor(answers): update fastify answer routes and remove empty lines

--- src/main/fastify/routes/answers/answer-question/answer-question.route.ts
+++ src/main/fastify/routes/answers/answer-question/answer-question.route.ts
@@ -1,12 +1,9 @@
 import type { FastifyInstance } from 'fastify'
-
 import {
   answerQuestionBodySchema,
   answerQuestionResponsesSchema
 } from '@/infra/validation/zod/schemas/presentation/answers/answer-question.schemas'
-
 import { makeAnswerQuestionController } from '@/main/factories/answer-question'
-
 import { adaptRoute } from '@/util/adapt-route'
 
 export async function answerQuestionRoute (app: FastifyInstance, tags: string[]) {
