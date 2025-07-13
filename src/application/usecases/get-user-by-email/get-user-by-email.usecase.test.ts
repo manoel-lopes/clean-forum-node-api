95c07edaf71a69dcefa452a6315245a74f4c03b1
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 16:39:52 -0300

    style(users): remove empty lines from get user by email use case

--- src/application/usecases/get-user-by-email/get-user-by-email.usecase.test.ts
+++ src/application/usecases/get-user-by-email/get-user-by-email.usecase.test.ts
@@ -1,33 +1,25 @@
 import { InMemoryUsersRepository } from '@/infra/persistence/repositories/in-memory/in-memory-users.repository'
-
 import type { UsersRepository } from '@/application/repositories/users.repository'
 import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
-
 import { makeUser } from '@/util/factories/domain/make-user'
-
 import { GetUserByEmailUseCase } from './get-user-by-email.usecase'
 
 describe('Get User By Email Use Case', () => {
   let usersRepository: UsersRepository
   let sut: GetUserByEmailUseCase
-
   beforeEach(() => {
     usersRepository = new InMemoryUsersRepository()
     sut = new GetUserByEmailUseCase(usersRepository)
   })
-
   it('should be able to get a user by email', async () => {
     const user = makeUser({ email: 'johndoe@example.com' })
     await usersRepository.save(user)
-
     const response = await sut.execute({ email: 'johndoe@example.com' })
-
     expect(response.id).toEqual(user.id)
     expect(response.name).toEqual(user.name)
     expect(response.email).toEqual(user.email)
     expect(response.createdAt).toEqual(user.createdAt)
   })
-
   it('should not be able to get a non-existing user by email', async () => {
     await expect(
       sut.execute({ email: 'nonexistent@example.com' })
--- src/application/usecases/get-user-by-email/get-user-by-email.usecase.ts
+++ src/application/usecases/get-user-by-email/get-user-by-email.usecase.ts
@@ -1,16 +1,12 @@
 import type { UseCase } from '@/core/application/use-case'
-
 import type { UsersRepository } from '@/application/repositories/users.repository'
 import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
-
 import type { User } from '@/domain/entities/user/user.entity'
 
 type GetUserByEmailUseCaseRequest = {
   email: string
 }
-
 type GetUserByEmailUseCaseResponse = Omit<User, 'password'>
-
 export class GetUserByEmailUseCase implements UseCase {
   constructor (private readonly usersRepository: UsersRepository) {}
 
@@ -21,7 +17,6 @@ export class GetUserByEmailUseCase implements UseCase {
     if (!user) {
       throw new ResourceNotFoundError('User')
     }
-
     return {
       id: user.id,
       name: user.name,
