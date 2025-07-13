93c4f29bf7ea32da0040daea92fc29e8274e36a0
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 16:42:12 -0300

    style(in-memory-repositories): remove empty lines and unused method from in-memory repositories

--- src/infra/persistence/repositories/in-memory/in-memory-questions.repository.ts
+++ src/infra/persistence/repositories/in-memory/in-memory-questions.repository.ts
@@ -1,13 +1,10 @@
 import type { PaginatedItems } from '@/core/application/paginated-items'
 import type { PaginationParams } from '@/core/application/pagination-params'
-
 import type {
   QuestionsRepository,
   UpdateQuestionData
 } from '@/application/repositories/questions.repository'
-
 import { Question } from '@/domain/entities/question/question.entity'
-
 import { BaseInMemoryRepository as BaseRepository } from './base/base-in-memory.repository'
 
 export class InMemoryQuestionsRepository
@@ -33,9 +30,7 @@ export class InMemoryQuestionsRepository
     const questions = this.items.slice(start, end)
     const totalItems = this.items.length
     const totalPages = Math.ceil(totalItems / requestedPageSize)
-
     const actualPageSize = Math.min(requestedPageSize, totalItems)
-
     return {
       page,
       pageSize: actualPageSize,
--- src/infra/persistence/repositories/in-memory/in-memory-users.repository.ts
+++ src/infra/persistence/repositories/in-memory/in-memory-users.repository.ts
@@ -1,10 +1,7 @@
 import type { PaginatedItems } from '@/core/application/paginated-items'
 import type { PaginationParams } from '@/core/application/pagination-params'
-
 import type { UsersRepository } from '@/application/repositories/users.repository'
-
 import type { User } from '@/domain/entities/user/user.entity'
-
 import { BaseInMemoryRepository as BaseRepository } from './base/base-in-memory.repository'
 
 export class InMemoryUsersRepository extends BaseRepository<User> implements UsersRepository {
@@ -19,9 +16,7 @@ export class InMemoryUsersRepository extends BaseRepository<User> implements Use
     const users = this.items.slice(start, end)
     const totalItems = this.items.length
     const totalPages = Math.ceil(totalItems / requestedPageSize)
-
     const actualPageSize = Math.min(requestedPageSize, totalItems)
-
     return {
       page,
       pageSize: actualPageSize,
