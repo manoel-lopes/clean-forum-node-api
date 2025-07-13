db4368f3b64e98d2cf5d8e484522ff5cfc820b90
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 16:35:26 -0300

    refactor(answers): remove fetch answers use case

--- src/main/factories/fetch-answers.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaAnswersRepository } from '@/infra/persistence/repositories/prisma/prisma-answers.repository'
-
-import { FetchAnswersController } from '@/presentation/controllers/fetch-answers/fetch-answers.controller'
-
-export const makeFetchAnswersController = (): FetchAnswersController => {
-  const answersRepository = new PrismaAnswersRepository()
-  const fetchAnswersController = new FetchAnswersController(answersRepository)
-  return fetchAnswersController
-}
