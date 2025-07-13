f23bc343c771a615475227bf2f2a82eba73902ca
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 16:51:04 -0300

    style(questions): remove empty lines from create question controller

--- src/presentation/controllers/create-question/create-question.controller.ts
+++ src/presentation/controllers/create-question/create-question.controller.ts
@@ -1,11 +1,8 @@
 import type { WebController } from '@/core/presentation/web-controller'
 import type { UseCase } from '@/core/application/use-case'
-
 import type { HttpRequest, HttpResponse } from '@/infra/http/ports/http-protocol'
-
 import { QuestionWithTitleAlreadyRegisteredError } from '@/application/usecases/create-question/errors/question-with-title-already-registered.error'
 import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
-
 import { conflict, created, notFound } from '@/presentation/helpers/http-helpers'
 
 export class CreateQuestionController implements WebController {
