847f1f22a1bb9e8c568ac62bbb58059850fe3054
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 16:51:31 -0300

    style(account): remove empty lines from delete account controller

--- src/presentation/controllers/delete-account/delete-account.controller.ts
+++ src/presentation/controllers/delete-account/delete-account.controller.ts
@@ -1,10 +1,7 @@
 import type { WebController } from '@/core/presentation/web-controller'
 import type { UseCase } from '@/core/application/use-case'
-
 import type { HttpRequest, HttpResponse } from '@/infra/http/ports/http-protocol'
-
 import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
-
 import { noContent, notFound } from '@/presentation/helpers/http-helpers'
 
 export class DeleteAccountController implements WebController {
