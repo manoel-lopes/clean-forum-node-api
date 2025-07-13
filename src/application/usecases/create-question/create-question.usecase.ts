a1424635e6642fad30fafea7c272380c85108193
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 16:38:12 -0300

    style(questions): remove empty lines from create question use case

--- src/application/usecases/create-question/create-question.usecase.ts
+++ src/application/usecases/create-question/create-question.usecase.ts
@@ -1,16 +1,12 @@
 import type { UseCase } from '@/core/application/use-case'
-
 import type { QuestionsRepository } from '@/application/repositories/questions.repository'
 import type { UsersRepository } from '@/application/repositories/users.repository'
 import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
-
 import type { QuestionProps } from '@/domain/entities/question/ports/question.props'
 import { Question } from '@/domain/entities/question/question.entity'
-
 import { QuestionWithTitleAlreadyRegisteredError } from './errors/question-with-title-already-registered.error'
 
 export type CreateQuestionRequest = QuestionProps
-
 export class CreateQuestionUseCase implements UseCase {
   constructor (
     private readonly questionsRepository: QuestionsRepository,
@@ -25,12 +21,10 @@ export class CreateQuestionUseCase implements UseCase {
     if (!author) {
       throw new ResourceNotFoundError('User')
     }
-
     const questionWithTitle = await this.questionsRepository.findByTitle(title)
     if (questionWithTitle) {
       throw new QuestionWithTitleAlreadyRegisteredError()
     }
-
     const question = Question.create({
       title,
       content,
