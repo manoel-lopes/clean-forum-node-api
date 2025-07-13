0a153001647ef29a43b6ed231e2cd316b20b65f6
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 16:36:12 -0300

    style: remove empty line from error files

--- src/application/errors/not-author.error.ts
+++ src/application/errors/not-author.error.ts
@@ -1,5 +1,4 @@
 type Resource = 'question' | 'answer'
-
 export class NotAuthorError extends Error {
   constructor (resource: Resource) {
     super(`The user is not the author of the ${resource}`)
