7cf1f422e9b63fcd4265f05bbc12f6f7f124c1a3
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 16:38:30 -0300

    style(account): remove empty lines from delete account use case

--- src/application/usecases/delete-account/delete-account.usecase.ts
+++ src/application/usecases/delete-account/delete-account.usecase.ts
@@ -1,12 +1,10 @@
 import type { UseCase } from '@/core/application/use-case'
-
 import type { UsersRepository } from '@/application/repositories/users.repository'
 import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
 
 export type DeleteAccountRequest = {
   userId: string
 }
-
 export class DeleteAccountUseCase implements UseCase {
   constructor (private readonly usersRepository: UsersRepository) {
     Object.freeze(this)
