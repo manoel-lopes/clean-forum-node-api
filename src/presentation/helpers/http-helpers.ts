7d6bff6af7f9d12670e63b11605441795e8e9d5a
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 16:59:07 -0300

    style(http): remove empty lines from http helper files

--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -1,40 +1,30 @@
 import type { HttpResponse, HttpStatusCode } from '@/infra/http/ports/http-protocol'
-
 import type { HttpError, HttpErrorType } from './errors/http.error'
 
 export const created = (): HttpResponse => ({ statusCode: 201 })
-
 export const noContent = (): HttpResponse => ({ statusCode: 204, body: null })
-
 export const ok = <T>(data: T): HttpResponse<T> => ({
   statusCode: 200,
   body: data
 })
-
 export const badRequest = (err: Error): HttpResponse => {
   return httpError({ name: 'Bad Request', message: err.message })
 }
-
 export const unauthorized = (err: Error): HttpResponse => {
   return httpError({ name: 'Unauthorized', message: err.message })
 }
-
 export const forbidden = (err: Error): HttpResponse => {
   return httpError({ name: 'Forbidden', message: err.message })
 }
-
 export const notFound = (err: Error): HttpResponse => {
   return httpError({ name: 'Not Found', message: err.message })
 }
-
 export const conflict = (err: Error): HttpResponse => {
   return httpError({ name: 'Conflict', message: err.message })
 }
-
 export const unprocessableEntity = (err: Error): HttpResponse => {
   return httpError({ name: 'Unprocessable Entity', message: err.message })
 }
-
 const httpError = (err: HttpError): HttpResponse => {
   const statusCodeMapper: Record<HttpErrorType, HttpStatusCode> = {
     'Bad Request': 400,
