014ff2756c2017590715f5e39dcb5dd446edbbf1
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-13 16:50:09 -0300

    style(questions): remove empty lines from choose question best answer controller

--- src/presentation/controllers/choose-question-best-answer/choose-question-best-answer.controller.ts
+++ src/presentation/controllers/choose-question-best-answer/choose-question-best-answer.controller.ts
@@ -1,11 +1,8 @@
 import type { WebController } from '@/core/presentation/web-controller'
 import type { UseCase } from '@/core/application/use-case'
-
 import type { HttpRequest, HttpResponse } from '@/infra/http/ports/http-protocol'
-
 import { NotAuthorError } from '@/application/errors/not-author.error'
 import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
-
 import { forbidden, notFound, ok } from '@/presentation/helpers/http-helpers'
 
 export class ChooseQuestionBestAnswerController implements WebController {
