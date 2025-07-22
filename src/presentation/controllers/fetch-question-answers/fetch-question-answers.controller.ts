00bcc662ff69af9b376adfaa68ac419a8eb705fd
manoel-lopes <manoel.lopes.full@gmail.com>
2025-07-22 19:18:24 -0300

    style: add newline in fetch-question-answers controller

--- src/presentation/controllers/fetch-question-answers/fetch-question-answers.controller.ts
+++ src/presentation/controllers/fetch-question-answers/fetch-question-answers.controller.ts
@@ -5,6 +5,7 @@ import { ok } from '@/presentation/helpers/http-helpers'
 
 export class FetchQuestionAnswersController implements WebController {
   constructor (private readonly answersRepository: AnswersRepository) {}
+
   async handle (req: HttpRequest): Promise<HttpResponse> {
     const page = req.query?.page ?? 1
     const pageSize = req.query?.pageSize ?? 20
