8e2119dd71c30d77cc1b454284fe09d8b7aa36ef
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-13 14:27:54 -0300

    fix(authentication): update controller and tests to handle token response

--- src/presentation/controllers/authenticate-user/authenticate-user.controller.test.ts
+++ src/presentation/controllers/authenticate-user/authenticate-user.controller.test.ts
@@ -2,27 +2,38 @@ import type { UseCase } from '@/core/application/use-case'
 import { UseCaseStub } from '@/infra/doubles/stubs/use-case.stub'
 import { InvalidPasswordError } from '@/application/usecases/authenticate-user/errors/invalid-password.error'
 import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
+import { makeUser } from '@/util/factories/domain/make-user'
 import { AuthenticateUserController } from './authenticate-user.controller'
 
 describe('AuthenticateUserController', () => {
   let sut: AuthenticateUserController
   let authenticateUserUseCase: UseCase
-
-  beforeEach(() => {
-    authenticateUserUseCase = new UseCaseStub()
-    sut = new AuthenticateUserController(authenticateUserUseCase)
-  })
   const httpRequest = {
     body: {
       email: 'any_email@mail.com',
       password: 'any_password'
     }
   }
+
+  vi.mock('@/lib/env', () => ({
+    env: {
+      NODE_ENV: 'development',
+      JWT_SECRET: 'any_secret'
+    }
+  }))
+
+  beforeEach(() => {
+    authenticateUserUseCase = new UseCaseStub()
+    sut = new AuthenticateUserController(authenticateUserUseCase)
+  })
+
   it('should return 404 code and an not found error response if the user is not found', async () => {
     vi.spyOn(authenticateUserUseCase, 'execute').mockRejectedValue(
       new ResourceNotFoundError('User')
     )
+
     const httpResponse = await sut.handle(httpRequest)
+
     expect(httpResponse.statusCode).toBe(404)
     expect(httpResponse.body).toEqual({
       error: 'Not Found',
@@ -34,7 +45,9 @@ describe('AuthenticateUserController', () => {
     vi.spyOn(authenticateUserUseCase, 'execute').mockRejectedValue(
       new InvalidPasswordError()
     )
+
     const httpResponse = await sut.handle(httpRequest)
+
     expect(httpResponse.statusCode).toBe(401)
     expect(httpResponse.body).toEqual({
       error: 'Unauthorized',
@@ -44,25 +57,34 @@ describe('AuthenticateUserController', () => {
 
   it('should throw an unknown error response if an unexpect error occur', async () => {
     const error = new Error('any_error')
+
     vi.spyOn(authenticateUserUseCase, 'execute').mockRejectedValue(error)
     await expect(sut.handle(httpRequest)).rejects.toThrow(error)
   })
 
   it('should return 200 and an ok response with the user data on successful authentication', async () => {
-    const user = {
-      id: 'any_id',
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      createdAt: new Date().toISOString()
-    }
-    vi.spyOn(authenticateUserUseCase, 'execute').mockResolvedValue(user)
+    const user = makeUser()
+    vi.spyOn(authenticateUserUseCase, 'execute').mockResolvedValue({
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: user.createdAt
+      },
+      token: 'any_token'
+    })
+
     const httpResponse = await sut.handle(httpRequest)
+
     expect(httpResponse.statusCode).toBe(200)
     expect(httpResponse.body).toEqual({
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      createdAt: user.createdAt
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: user.createdAt
+      },
+      token: 'any_token'
     })
   })
 })
