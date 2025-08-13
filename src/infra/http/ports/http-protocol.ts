ed14008ff9b0f9151508eb586e0dd495c7e6fbd5
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-13 13:09:56 -0300

    feat(http): add http headers to http request protocol

--- src/infra/http/ports/http-protocol.ts
+++ src/infra/http/ports/http-protocol.ts
@@ -1,12 +1,19 @@
 export type HttpMethod = 'POST' | 'GET' | 'PUT' | 'PATCH' | 'DELETE'
 export type HttpStatusCode = 200 | 201 | 204 | 400 | 401 | 403 | 404 | 409 | 422 | 500
 export type HttpRedirectStatusCode = 301 | 302
+
+export type HttpHeaders = {
+  authorization?: string
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type HttpRequest<Body = any, Params = any, Query = any> = {
   body?: Body
   params?: Params
   query?: Query
+  headers?: HttpHeaders
 }
+
 export type HttpResponse<Body = unknown> = {
   statusCode: HttpStatusCode
   body?: Body
