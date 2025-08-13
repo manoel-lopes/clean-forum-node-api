4e64a1865222efaee24dcb1475eec3b40e946a89
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-13 13:04:37 -0300

    feat(fastify): add ensureAuthenticated middleware

---
+++ src/main/fastify/middlewares/ensure-authenticated.ts
@@ -0,0 +1,19 @@
+import { type FastifyReply, type FastifyRequest } from 'fastify'
+import { JWTService } from '@/infra/jwt-service'
+
+export async function ensureAuthenticated (
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const authToken = request.headers.authorization
+  if (!authToken) {
+    return reply.code(401).send({ message: 'Token is missing' })
+  }
+
+  const [, token] = authToken.split(' ')
+  try {
+    JWTService.verify(token)
+  } catch {
+    return reply.code(401).send({ message: 'Token invalid' })
+  }
+}
