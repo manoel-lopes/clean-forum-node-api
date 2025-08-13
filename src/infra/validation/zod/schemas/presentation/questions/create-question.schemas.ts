d5023c4198b03a3eefabc9198082d6063f8abdf6
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-13 13:02:26 -0300

    refactor(schemas): remove authorId from request body schemas

--- src/infra/validation/zod/schemas/presentation/questions/create-question.schemas.ts
+++ src/infra/validation/zod/schemas/presentation/questions/create-question.schemas.ts
@@ -4,8 +4,8 @@ import { errorResponseSchema } from '../../core/error-response.schema'
 export const createQuestionBodySchema = z.object({
   title: z.string().min(1),
   content: z.string().min(1),
-  authorId: z.string().uuid()
 })
+
 export const createQuestionResponsesSchema = {
   201: z.null(),
   400: errorResponseSchema,
