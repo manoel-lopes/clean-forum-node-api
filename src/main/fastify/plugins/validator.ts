ad22db3b4ca6721220ac6213f3a86736edac1eab
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-14 15:18:47 -0300

    chore: update miscellaneous files

--- src/main/fastify/plugins/validator.ts
+++ src/main/fastify/plugins/validator.ts
@@ -1,7 +1,13 @@
 import type { Schema } from 'zod'
 import type { FastifySchemaCompiler } from 'fastify'
-import { ZodSchemaParser } from '@/infra/validation/zod/helpers/zod-schema-parser'
 
 export const validatorCompiler: FastifySchemaCompiler<Schema> = ({ schema }) => {
-  return (data: unknown) => ZodSchemaParser.parse(schema, data)
+  return (data: unknown) => {
+    const result = schema.safeParse(data)
+    if (result.success) {
+      return { value: result.data }
+    } else {
+      return { error: result.error }
+    }
+  }
 }
