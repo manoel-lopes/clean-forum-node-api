332e8fb9c402aa476b6e37f4d00133db7146d9ce
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-14 15:19:29 -0300

    feat(api): add choose question best answer functionality

---
+++ src/main/fastify/routes/questions/choose-question-best-answer/choose-question-best-answer.route.ts
@@ -0,0 +1,17 @@
+import type { FastifyInstance } from 'fastify'
+import { chooseQuestionBestAnswerParamsSchema, chooseQuestionBestAnswerResponsesSchema } from '@/infra/validation/zod/schemas/presentation/questions/choose-question-best-answer.schemas'
+import { makeChooseQuestionBestAnswerController } from '@/main/factories/choose-question-best-answer'
+import { adaptRoute } from '@/util/adapt-route'
+
+export async function chooseQuestionBestAnswerRoute (app: FastifyInstance, tags: string[]) {
+  app.patch('/:answerId/choose', {
+    schema: {
+      tags,
+      description: 'Choose the best answer for an question',
+      params: chooseQuestionBestAnswerParamsSchema,
+      response: chooseQuestionBestAnswerResponsesSchema
+    }
+  },
+  adaptRoute(makeChooseQuestionBestAnswerController())
+  )
+}
--- src/presentation/controllers/choose-question-best-answer/choose-question-best-answer.controller.ts
+++ src/presentation/controllers/choose-question-best-answer/choose-question-best-answer.controller.ts
@@ -1,6 +1,6 @@
 import type { WebController } from '@/core/presentation/web-controller'
 import type { UseCase } from '@/core/application/use-case'
-import { JWTService } from '@/infra/jwt-service'
+import { JWTService } from '@/infra/auth/jwt/jwt-service'
 import type { HttpRequest, HttpResponse } from '@/infra/http/ports/http-protocol'
 import { NotAuthorError } from '@/application/errors/not-author.error'
 import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
