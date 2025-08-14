1337a4018c015bfd7702ea6456a9e2dd1938b2d5
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-14 15:20:23 -0300

    refactor(routes): update answer routes

--- src/main/fastify/routes/answers/answers.routes.ts
+++ src/main/fastify/routes/answers/answers.routes.ts
@@ -1,7 +1,6 @@
 import type { FastifyInstance } from 'fastify'
 import { registerRoutes } from '@/main/fastify/helpers/register-routes'
 import { answerQuestionRoute } from './answer-question/answer-question.route'
-import { chooseQuestionBestAnswerRoute } from './choose-question-best-answer/choose-question-best-answer.route'
 import { deleteAnswerRoute } from './delete-answer/delete-answer.route'
 import { fetchQuestionAnswersRoute } from './fetch-question-answers/fetch-question-answers.route'
 
@@ -9,7 +8,6 @@ export async function answersRoutes (app: FastifyInstance) {
   registerRoutes(app, [
     answerQuestionRoute,
     deleteAnswerRoute,
-    chooseQuestionBestAnswerRoute,
     fetchQuestionAnswersRoute
   ])
 }
