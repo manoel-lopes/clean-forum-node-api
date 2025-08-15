fbad9dacb1e785f80cb71e11663893b271653d44
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-15 12:16:53 -0300

    style: adjust JWT_SECRET mock in answer and delete controller tests

--- src/presentation/controllers/answer-question/answer-question.controller.test.ts
+++ src/presentation/controllers/answer-question/answer-question.controller.test.ts
@@ -4,6 +4,13 @@ import { UseCaseStub } from '@/infra/doubles/stubs/use-case.stub'
 import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
 import { AnswerQuestionController } from './answer-question.controller'
 
+vi.mock('@/lib/env', () => ({
+  env: {
+    NODE_ENV: 'development',
+    JWT_SECRET: 'any_secret'
+  }
+}))
+
 describe('AnswerQuestionController', () => {
   let sut: AnswerQuestionController
   let answerQuestionUseCase: UseCase
@@ -19,23 +26,19 @@ describe('AnswerQuestionController', () => {
     }
   }
 
-  vi.mock('@/lib/env', () => ({
-    env: {
-      NODE_ENV: 'development',
-      JWT_SECRET: 'any_secret'
-    }
-  }))
-
   beforeEach(() => {
     answerQuestionUseCase = new UseCaseStub()
     sut = new AnswerQuestionController(answerQuestionUseCase)
     vi.spyOn(JWTService, 'decodeToken').mockReturnValue({ sub: userId })
   })
+
   it('should return 404 code and an not found error response if the author is not found', async () => {
     vi.spyOn(answerQuestionUseCase, 'execute').mockRejectedValue(
       new ResourceNotFoundError('User')
     )
+
     const httpResponse = await sut.handle(httpRequest)
+
     expect(httpResponse.statusCode).toBe(404)
     expect(httpResponse.body).toEqual({
       error: 'Not Found',
@@ -46,11 +49,13 @@ describe('AnswerQuestionController', () => {
   it('should throw an unknown error response if an unexpect error occur', async () => {
     const error = new Error('any_error')
     vi.spyOn(answerQuestionUseCase, 'execute').mockRejectedValue(error)
+
     await expect(sut.handle(httpRequest)).rejects.toThrow(error)
   })
 
   it('should return a created response on the creation of an answer', async () => {
     const httpResponse = await sut.handle(httpRequest)
+
     expect(httpResponse.statusCode).toBe(201)
   })
 })
