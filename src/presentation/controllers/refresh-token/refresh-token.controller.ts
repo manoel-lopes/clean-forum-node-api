37105e6376dfe83313665e8d1d9b75e6e04b3ab2
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-15 12:17:09 -0300

    feat(auth): handle expired refresh token and resource not found errors

--- src/presentation/controllers/refresh-token/refresh-token.controller.ts
+++ src/presentation/controllers/refresh-token/refresh-token.controller.ts
@@ -1,7 +1,9 @@
 import type { WebController } from '@/core/presentation/web-controller'
 import type { UseCase } from '@/core/application/use-case'
 import type { HttpRequest, HttpResponse } from '@/infra/http/ports/http-protocol'
-import { badRequest, ok } from '@/presentation/helpers/http-helpers'
+import { ExpiredRefreshTokenError } from '@/application/usecases/refresh-token/errors/expired-refresh-token.error'
+import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
+import { badRequest, notFound, ok } from '@/presentation/helpers/http-helpers'
 
 export class RefreshAccessTokenController implements WebController {
   constructor (private readonly refreshAccessTokenUseCase: UseCase) {}
@@ -12,7 +14,13 @@ export class RefreshAccessTokenController implements WebController {
       const response = await this.refreshAccessTokenUseCase.execute({ refreshTokenId })
       return ok(response)
     } catch (error) {
-      return badRequest(error)
+      if (error instanceof ResourceNotFoundError) {
+        return notFound(error)
+      }
+      if (error instanceof ExpiredRefreshTokenError) {
+        return badRequest(error)
+      }
+      throw error
     }
   }
 }
