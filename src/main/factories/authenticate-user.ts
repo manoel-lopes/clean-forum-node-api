e47c5cf087f7683b94e0d412a9e670883d6f8384
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-16 13:51:04 -0300

    refactor(auth): update prisma refresh tokens repository import

--- src/main/factories/authenticate-user.ts
+++ src/main/factories/authenticate-user.ts
@@ -1,6 +1,6 @@
 import type { WebController } from '@/core/presentation/web-controller'
 import { PasswordHasherStub } from '@/infra/adapters/security/stubs/password-hasher.stub'
-import { PrismaRefreshTokensRepository } from '@/infra/persistence/prisma/repositories/prisma-refresh-tokens.repository'
+import { PrismaRefreshTokensRepository } from '@/infra/persistence/repositories/prisma/prisma-refresh-tokens.repository'
 import { PrismaUsersRepository } from '@/infra/persistence/repositories/prisma/prisma-users.repository'
 import { AuthenticateUserUseCase } from '@/application/usecases/authenticate-user/authenticate-user.usecase'
 import { AuthenticateUserController } from '@/presentation/controllers/authenticate-user/authenticate-user.controller'
