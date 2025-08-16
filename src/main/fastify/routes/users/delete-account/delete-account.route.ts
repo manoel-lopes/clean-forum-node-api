e3a0b3aa4f676b57ec5aba3acad6913921ca8865
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-16 13:50:34 -0300

    refactor(delete-account): update controller to use JWT for user ID

--- src/main/fastify/routes/users/delete-account/delete-account.route.ts
+++ src/main/fastify/routes/users/delete-account/delete-account.route.ts
@@ -1,17 +1,15 @@
 import type { FastifyInstance } from 'fastify'
 import {
-  deleteAccountParamsSchema,
   deleteAccountResponsesSchema
 } from '@/infra/validation/zod/schemas/presentation/users/delete-account.schemas'
 import { makeDeleteAccountController } from '@/main/factories/delete-account'
 import { adaptRoute } from '@/util/adapt-route'
 
 export async function deleteAccountRoute (app: FastifyInstance, tags: string[]) {
-  app.delete('/:userId', {
+  app.delete('/', {
     schema: {
       tags,
       description: 'Delete a user account',
-      params: deleteAccountParamsSchema,
       response: deleteAccountResponsesSchema
     }
   },
