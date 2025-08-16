280f560df4a5d04c65f3c19dfa2a1f368d269b47
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-16 13:50:18 -0300

    refactor(delete-account): remove resource not found check from use case

--- src/application/usecases/delete-account/delete-account.usecase.ts
+++ src/application/usecases/delete-account/delete-account.usecase.ts
@@ -1,11 +1,11 @@
 import type { UseCase } from '@/core/application/use-case'
 import type { RefreshTokensRepository } from '@/application/repositories/refresh-tokens.repository'
 import type { UsersRepository } from '@/application/repositories/users.repository'
-import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
 
 export type DeleteAccountRequest = {
   userId: string
 }
+
 export class DeleteAccountUseCase implements UseCase {
   constructor (
     private readonly usersRepository: UsersRepository,
@@ -16,10 +16,6 @@ export class DeleteAccountUseCase implements UseCase {
 
   async execute (req: DeleteAccountRequest): Promise<void> {
     const { userId } = req
-    const user = await this.usersRepository.findById(userId)
-    if (!user) {
-      throw new ResourceNotFoundError('User')
-    }
     await this.refreshTokensRepository.deleteManyByUserId(userId)
     await this.usersRepository.delete(userId)
   }
