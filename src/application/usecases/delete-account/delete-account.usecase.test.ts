be63491fed8ae5daca1f68548277e16769d3ed65
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-16 13:50:50 -0300

    test(delete-account): update tests for delete account use case and controller

--- src/application/usecases/delete-account/delete-account.usecase.test.ts
+++ src/application/usecases/delete-account/delete-account.usecase.test.ts
@@ -2,7 +2,6 @@ import type { RefreshTokensRepository } from '@/application/repositories/refresh
 import type { UsersRepository } from '@/application/repositories/users.repository'
 import { InMemoryRefreshTokensRepository } from '@/infra/persistence/repositories/in-memory/in-memory-refresh-tokens.repository'
 import { InMemoryUsersRepository } from '@/infra/persistence/repositories/in-memory/in-memory-users.repository'
-import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
 import { makeRefreshToken } from '@/util/factories/domain/make-refresh-token'
 import { makeUser } from '@/util/factories/domain/make-user'
 import { DeleteAccountUseCase } from './delete-account.usecase'
@@ -16,12 +15,6 @@ describe('DeleteAccountUseCase', () => {
     usersRepository = new InMemoryUsersRepository()
     refreshTokensRepository = new InMemoryRefreshTokensRepository()
     sut = new DeleteAccountUseCase(usersRepository, refreshTokensRepository)
-  })
-
-  it('should not delete a nonexistent user account', async () => {
-    await expect(sut.execute({
-      userId: 'any_inexistent_id'
-    })).rejects.toThrowError(new ResourceNotFoundError('User'))
   })
 
   it('should delete a user account', async () => {
@@ -39,11 +32,18 @@ describe('DeleteAccountUseCase', () => {
   it('should delete the refresh token when deleting a user account', async () => {
     const user = makeUser()
     await usersRepository.save(user)
-    const refreshToken = makeRefreshToken({ userId: user.id })
-    await refreshTokensRepository.save(refreshToken)
-    const createdRefreshToken = await refreshTokensRepository.findByUserId(user.id)
-    expect(createdRefreshToken).not.toBeNull()
+    await refreshTokensRepository.save(makeRefreshToken({ userId: user.id }))
+    const refreshToken = await refreshTokensRepository.findByUserId(user.id)
+    const userAccount = await usersRepository.findById(user.id)
+    expect(userAccount?.id).toBe(user.id)
+    expect(userAccount?.name).toBe(user.name)
+    expect(userAccount?.email).toBe(user.email)
+    expect(refreshToken).not.toBeNull()
+
     await sut.execute({ userId: user.id })
+
+    const deletedAccount = await usersRepository.findById(user.id)
+    expect(deletedAccount).toBeNull()
     const deletedRefreshToken = await refreshTokensRepository.findByUserId(user.id)
     expect(deletedRefreshToken).toBeNull()
   })
