fe2a8373798db2682a954e48541a3c873d6f6521
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-16 13:50:34 -0300

    refactor(delete-account): update controller to use JWT for user ID

--- src/presentation/controllers/delete-account/delete-account.controller.ts
+++ src/presentation/controllers/delete-account/delete-account.controller.ts
@@ -1,15 +1,19 @@
 import type { WebController } from '@/core/presentation/web-controller'
 import type { UseCase } from '@/core/application/use-case'
+import { JWTService } from '@/infra/auth/jwt/jwt-service'
 import type { HttpRequest, HttpResponse } from '@/infra/http/ports/http-protocol'
 import { ResourceNotFoundError } from '@/application/errors/resource-not-found.error'
 import { noContent, notFound } from '@/presentation/helpers/http-helpers'
+import { extractToken } from '@/util/auth/extract-token'
 
 export class DeleteAccountController implements WebController {
   constructor (private readonly deleteAccountUseCase: UseCase) {}
 
   async handle (req: HttpRequest): Promise<HttpResponse> {
     try {
-      const { userId } = req.params
+      const token = extractToken(req.headers?.authorization)
+      const decodedToken = JWTService.decodeToken(token)
+      const { sub: userId } = decodedToken
       await this.deleteAccountUseCase.execute({ userId })
       return noContent()
     } catch (error) {
