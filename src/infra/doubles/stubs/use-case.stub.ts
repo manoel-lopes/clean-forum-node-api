f09b76b918b263335a53a41ee2f6447f7071518d
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-18 00:40:43 -0300

    refactor(infra): remove unused stub files

--- src/infra/doubles/stubs/use-case.stub.ts
+++
@@ -1,6 +0,0 @@
-import { vi } from 'vitest'
-import type { UseCase } from '@/core/application/use-case'
-
-export class UseCaseStub implements UseCase {
-  execute = vi.fn(async () => Promise.resolve())
-}
