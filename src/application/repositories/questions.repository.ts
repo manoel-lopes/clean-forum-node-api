c16c3ca0e2a8659c7e64463db0f1d88c344e76fd
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-19 11:25:50 -0300

    feat(questions): fetch paginated answers with question

--- src/application/repositories/questions.repository.ts
+++ src/application/repositories/questions.repository.ts
@@ -1,5 +1,6 @@
 import type { PaginatedItems } from '@/core/application/paginated-items'
 import type { PaginationParams } from '@/core/application/pagination-params'
+import type { Answer } from '@/domain/entities/answer/answer.entity'
 import type { Question } from '@/domain/entities/question/question.entity'
 
 export type UpdateQuestionData = {
@@ -8,11 +9,20 @@ export type UpdateQuestionData = {
   content?: string
   bestAnswerId?: string
 }
+
+export type FindQuestionBySlugParams = PaginationParams & {
+  slug?: string
+}
+
+export type FindQuestionsResult = Omit<Question, 'answers'> & {
+  answers: PaginatedItems<Answer>
+} | null
+
 export type QuestionsRepository = {
   save(question: Question): Promise<void>
   findById(questionId: string): Promise<Question | null>
-  findByTitle(title: string): Promise<Question | null>
-  findBySlug(slug: string): Promise<Question | null>
+  findByTitle(questionTitle: string): Promise<Question | null>
+  findBySlug(params: FindQuestionBySlugParams): Promise<FindQuestionsResult | null>
   delete(questionId: string): Promise<void>
   update(questionData: UpdateQuestionData): Promise<Question>
   findMany(paginationParams: PaginationParams): Promise<PaginatedItems<Question>>
