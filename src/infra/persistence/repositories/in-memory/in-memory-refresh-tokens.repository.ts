e7852c6ce4e37abe7e1d85fed0bc30522a3736c6
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-23 19:08:57 -0300

    refactor(in-memory): extend refresh tokens repository with base functionality The InMemoryRefreshTokensRepository now extends the BaseInMemoryRepository, simplifying the implementation by leveraging inherited methods for saving, finding, and deleting refresh tokens. This change reduces code duplication and enhances maintainability.

--- src/infra/persistence/repositories/in-memory/in-memory-refresh-tokens.repository.ts
+++ src/infra/persistence/repositories/in-memory/in-memory-refresh-tokens.repository.ts
@@ -1,32 +1,13 @@
 import type { RefreshTokensRepository } from '@/application/repositories/refresh-tokens.repository'
 import { RefreshToken } from '@/domain/entities/refresh-token/refresh-token.entity'
+import { BaseInMemoryRepository as BaseRepository } from './base/base-in-memory.repository'
 
-export class InMemoryRefreshTokensRepository implements RefreshTokensRepository {
-  public items: RefreshToken[] = []
-
-  async save (refreshToken: RefreshToken): Promise<void> {
-    this.items.push(refreshToken)
-  }
-
-  async findById (id: string): Promise<RefreshToken | null> {
-    const refreshToken = this.items.find(item => item.id === id)
-    return refreshToken || null
-  }
-
+export class InMemoryRefreshTokensRepository extends BaseRepository<RefreshToken> implements RefreshTokensRepository {
   async findByUserId (userId: string): Promise<RefreshToken | null> {
-    const refreshToken = this.items.find(item => item.userId === userId)
-    return refreshToken || null
-  }
-
-  async delete (id: string): Promise<void> {
-    this.items = this.items.filter(item => item.id !== id)
+    return this.findOneBy('userId', userId)
   }
 
   async deleteManyByUserId (userId: string): Promise<void> {
     this.items = this.items.filter(item => item.userId !== userId)
-  }
-
-  async revoke (id: string): Promise<void> {
-    this.items = this.items.filter(item => item.id !== id)
   }
 }
