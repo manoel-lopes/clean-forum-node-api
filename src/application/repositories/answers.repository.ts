6abe494e8e645e454b4f0e93695cf901c889cfc0
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-23 18:32:24 -0300

    feat(answers): add update method to answers repository

--- src/application/repositories/answers.repository.ts
+++ src/application/repositories/answers.repository.ts
@@ -4,8 +4,15 @@ import type { Answer } from '@/domain/entities/answer/answer.entity'
 export type FindManyByQuestionIdParams = PaginationParams & {
   questionId: string
 }
+
+export type UpdateAnswerData = {
+  where: { id: string }
+  data: Partial<Omit<Answer, 'id' | 'createdAt' | 'updatedAt'>>
+}
+
 export type AnswersRepository = {
   save: (answer: Answer) => Promise<void>
   findById(answerId: string): Promise<Answer | null>
   delete: (answerId: string) => Promise<void>
+  update: (answerData: UpdateAnswerData) => Promise<Answer>
 }
