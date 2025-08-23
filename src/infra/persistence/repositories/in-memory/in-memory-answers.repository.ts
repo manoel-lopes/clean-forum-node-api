60adc927d133e3af2a937d0442f192a8f8559d69
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-23 18:32:41 -0300

    feat(answers): implement update method in in-memory repository

--- src/infra/persistence/repositories/in-memory/in-memory-answers.repository.ts
+++ src/infra/persistence/repositories/in-memory/in-memory-answers.repository.ts
@@ -1,7 +1,13 @@
 import type {
-  AnswersRepository
+  AnswersRepository,
+  UpdateAnswerData
 } from '@/application/repositories/answers.repository'
 import type { Answer } from '@/domain/entities/answer/answer.entity'
 import { BaseInMemoryRepository as BaseRepository } from './base/base-in-memory.repository'
 
-export class InMemoryAnswersRepository extends BaseRepository<Answer> implements AnswersRepository {}
+export class InMemoryAnswersRepository extends BaseRepository<Answer> implements AnswersRepository {
+  async update (answer: UpdateAnswerData): Promise<Answer> {
+    const updatedAnswer = await this.updateOne({ id: answer.where.id, ...answer.data })
+    return updatedAnswer
+  }
+}
