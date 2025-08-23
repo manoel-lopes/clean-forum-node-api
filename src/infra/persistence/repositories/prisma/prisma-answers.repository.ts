f7a7c901d703c12e538c9d8d75d04611f106d32f
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-23 18:32:58 -0300

    feat(answers): implement update method in prisma repository

--- src/infra/persistence/repositories/prisma/prisma-answers.repository.ts
+++ src/infra/persistence/repositories/prisma/prisma-answers.repository.ts
@@ -1,4 +1,4 @@
-import type { AnswersRepository } from '@/application/repositories/answers.repository'
+import type { AnswersRepository, UpdateAnswerData } from '@/application/repositories/answers.repository'
 import { PrismaAnswerMapper } from '@/infra/persistence/mappers/prisma/prisma-answer.mapper'
 import { prisma } from '@/infra/persistence/prisma/client'
 import type { Answer } from '@/domain/entities/answer/answer.entity'
@@ -20,5 +20,18 @@ export class PrismaAnswersRepository implements AnswersRepository {
     await prisma.answer.delete({
       where: { id: answerId },
     })
+  }
+
+  async update ({ data, where }: UpdateAnswerData): Promise<Answer> {
+    const updatedAnswer = await prisma.answer.update({
+      where: {
+        id: where.id,
+      },
+      data: {
+        content: data.content,
+      },
+    })
+
+    return PrismaAnswerMapper.toDomain(updatedAnswer)
   }
 }
