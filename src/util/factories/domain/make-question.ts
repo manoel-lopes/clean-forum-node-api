5149e549829b2d9ccb6fc43cea6f0665d4a95fe3
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-25 13:25:35 -0300

    refactor(question): refactor makeQuestion factory

--- src/util/factories/domain/make-question.ts
+++ src/util/factories/domain/make-question.ts
@@ -3,9 +3,10 @@ import { faker } from '@faker-js/faker'
 
 export function makeQuestion (override: Partial<Question> = {}): Question {
   const question = Question.create({
-    title: faker.lorem.sentence(),
+    title: override.slug ?? faker.lorem.sentence(),
     content: faker.lorem.paragraphs(),
-    authorId: faker.string.uuid()
+    authorId: faker.string.uuid(),
+    ...override,
   })
-  return Object.assign(question, override)
+  return question
 }
