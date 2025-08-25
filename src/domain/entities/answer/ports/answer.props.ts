d67bb4dc7701e0c00c1466615420b9bd97b8c74c
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-25 13:21:00 -0300

    refactor(answer): use Optional type for AnswerProps

--- src/domain/entities/answer/ports/answer.props.ts
+++ src/domain/entities/answer/ports/answer.props.ts
@@ -1,4 +1,4 @@
-import type { OmitIdAndTimestamps } from '@/util/types/omit-id-and-timestamps'
+import type { Optional } from '@/util/types/optional'
 import type { Answer } from '../answer.entity'
 
-export type AnswerProps = OmitIdAndTimestamps<Omit<Answer, 'excerpt'>>
+export type AnswerProps = Optional<Omit<Answer, 'id' | 'excerpt'>, 'createdAt' | 'updatedAt'>
