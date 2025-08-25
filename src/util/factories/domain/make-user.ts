7a432c1998d5758482f185bff7c2a90d674bd9e6
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-25 13:25:58 -0300

    refactor(user): refactor makeUser factory

--- src/util/factories/domain/make-user.ts
+++ src/util/factories/domain/make-user.ts
@@ -1,11 +1,12 @@
 import { User } from '@/domain/entities/user/user.entity'
 import { faker } from '@faker-js/faker'
 
-export function makeUser (override?: Partial<User>): User {
-  const baseUser = User.create({
+export function makeUser (override: Partial<User> = {}): User {
+  const user = User.create({
     name: faker.person.fullName(),
     email: faker.internet.email(),
-    password: faker.internet.password()
+    password: faker.internet.password(),
+    ...override,
   })
-  return { ...baseUser, ...override }
+  return user
 }
