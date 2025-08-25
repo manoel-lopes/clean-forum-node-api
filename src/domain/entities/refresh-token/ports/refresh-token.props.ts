cdcb3d4e8edf55136df954522b3d56a41d40e8a0
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-25 13:22:05 -0300

    refactor(refreshToken): use Optional type for RefreshTokenProps and make expiresAt optional

--- src/domain/entities/refresh-token/ports/refresh-token.props.ts
+++ src/domain/entities/refresh-token/ports/refresh-token.props.ts
@@ -1,3 +1,4 @@
-export interface RefreshTokenProps {
-  userId: string
-}
+import type { Optional } from '@/util/types/optional'
+import type { RefreshToken } from '../refresh-token.entity'
+
+export type RefreshTokenProps = Optional<Omit<RefreshToken, 'id'>, 'createdAt' | 'updatedAt' | 'expiresAt'>
