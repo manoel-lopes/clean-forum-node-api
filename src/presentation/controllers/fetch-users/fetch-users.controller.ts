ea883c6758f49aec86b793c1a0b33f620248004a
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-28 14:18:22 -0300

    refactor(presentation): remove user presenter from fetch users controller

--- src/presentation/controllers/fetch-users/fetch-users.controller.ts
+++ src/presentation/controllers/fetch-users/fetch-users.controller.ts
@@ -2,7 +2,6 @@ import type { WebController } from '@/core/presentation/web-controller'
 import type { UsersRepository } from '@/application/repositories/users.repository'
 import type { HttpRequest, HttpResponse } from '@/infra/http/ports/http-protocol'
 import { ok } from '@/presentation/helpers/http-helpers'
-import { UserPresenter } from '@/presentation/presenters/user.presenter'
 
 export class FetchUsersController implements WebController {
   constructor (private readonly usersRepository: UsersRepository) {}
@@ -10,6 +9,6 @@ export class FetchUsersController implements WebController {
   async handle (req: HttpRequest): Promise<HttpResponse> {
     const { page, pageSize, order } = req.query
     const response = await this.usersRepository.findMany({ page, pageSize, order })
-    return ok(UserPresenter.toHTTP(response))
+    return ok(response)
   }
 }
