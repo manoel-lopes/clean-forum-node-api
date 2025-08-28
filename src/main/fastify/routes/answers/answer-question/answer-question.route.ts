3464d9911e35147167b11984b31fe191232218ca
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-28 14:17:09 -0300

    refactor(main): remove questionId from route params

--- src/main/fastify/routes/answers/answer-question/answer-question.route.ts
+++ src/main/fastify/routes/answers/answer-question/answer-question.route.ts
@@ -1,18 +1,16 @@
 import type { FastifyInstance } from 'fastify'
 import {
   answerQuestionBodySchema,
-  answerQuestionParamsSchema,
   answerQuestionResponsesSchema
 } from '@/infra/validation/zod/schemas/presentation/answers/answer-question.schemas'
 import { makeAnswerQuestionController } from '@/main/factories/answer-question'
 import { adaptRoute } from '@/util/adapt-route'
 
 export async function answerQuestionRoute (app: FastifyInstance, tags: string[]) {
-  app.post('/:questionId', {
+  app.post('/', {
     schema: {
       tags,
       description: 'Answer a question',
-      params: answerQuestionParamsSchema,
       body: answerQuestionBodySchema,
       response: answerQuestionResponsesSchema
     },
