bf2d0fc508d7ebfb29260390a19f4f5fd5169850
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-29 13:42:06 -0300

    refactor(persistence): adjust cached repositories factory

--- src/infra/persistence/factories/cached-repositories.factory.ts
+++ src/infra/persistence/factories/cached-repositories.factory.ts
@@ -1,8 +1,13 @@
 import { RedisService } from '@/infra/providers/cache/redis-service'
-import { CachedAnswersRepository } from '../repositories/cached/cached-answers.repository'
+import { CachedAnswerCommentsRepository } from '../repositories/cached/cached-answer-comments.repository'
+import { CachedQuestionCommentsRepository } from '../repositories/cached/cached-question-comments.repository'
+import { CachedRefreshTokensRepository } from '../repositories/cached/cached-refresh-tokens.repository'
 import { CachedUsersRepository } from '../repositories/cached/cached-users.repository'
+import { PrismaAnswerCommentsRepository } from '../repositories/prisma/prisma-answer-comments.repository'
 import { PrismaAnswersRepository } from '../repositories/prisma/prisma-answers.repository'
+import { PrismaQuestionCommentsRepository } from '../repositories/prisma/prisma-question-comments.repository'
 import { PrismaQuestionsRepository } from '../repositories/prisma/prisma-questions.repository'
+import { PrismaRefreshTokensRepository } from '../repositories/prisma/prisma-refresh-tokens.repository'
 import { PrismaUsersRepository } from '../repositories/prisma/prisma-users.repository'
 
 export abstract class CachedRepositoriesFactory {
@@ -18,7 +23,21 @@ export abstract class CachedRepositoriesFactory {
   }
 
   static createAnswersRepository () {
-    const answersRepository = new PrismaAnswersRepository()
-    return new CachedAnswersRepository(this.redis, answersRepository)
+    return new PrismaAnswersRepository()
+  }
+
+  static createQuestionCommentsRepository () {
+    const questionCommentsRepository = new PrismaQuestionCommentsRepository()
+    return new CachedQuestionCommentsRepository(this.redis, questionCommentsRepository)
+  }
+
+  static createAnswerCommentsRepository () {
+    const answerCommentsRepository = new PrismaAnswerCommentsRepository()
+    return new CachedAnswerCommentsRepository(this.redis, answerCommentsRepository)
+  }
+
+  static createRefreshTokensRepository () {
+    const refreshTokensRepository = new PrismaRefreshTokensRepository()
+    return new CachedRefreshTokensRepository(this.redis, refreshTokensRepository)
   }
 }
