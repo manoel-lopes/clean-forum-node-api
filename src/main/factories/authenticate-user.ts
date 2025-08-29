eb3621a9ae0695918320e889e85125dec8e99901
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-29 13:43:03 -0300

    refactor(factories): use cached repository for refresh tokens

--- src/main/factories/authenticate-user.ts
+++ src/main/factories/authenticate-user.ts
@@ -1,14 +1,13 @@
 import type { WebController } from '@/core/presentation/web-controller'
 import { PasswordHasherStub } from '@/infra/adapters/security/stubs/password-hasher.stub'
 import { CachedRepositoriesFactory } from '@/infra/persistence/factories/cached-repositories.factory'
-import { PrismaRefreshTokensRepository } from '@/infra/persistence/repositories/prisma/prisma-refresh-tokens.repository'
 import { AuthenticateUserUseCase } from '@/application/usecases/authenticate-user/authenticate-user.usecase'
 import { AuthenticateUserController } from '@/presentation/controllers/authenticate-user/authenticate-user.controller'
 
 export function makeAuthenticateUserController (): WebController {
   const usersRepository = CachedRepositoriesFactory.createUsersRepository()
   const passwordHasher = new PasswordHasherStub()
-  const refreshTokensRepository = new PrismaRefreshTokensRepository()
+  const refreshTokensRepository = CachedRepositoriesFactory.createRefreshTokensRepository()
   const authenticateUserUseCase = new AuthenticateUserUseCase(usersRepository, passwordHasher, refreshTokensRepository)
   return new AuthenticateUserController(authenticateUserUseCase)
 }
