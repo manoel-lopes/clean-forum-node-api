7f3e18ab4540197e0bc81f5e0085bf27b5c6a3ff
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-31 13:10:41 -0300

    feat(test): add builder pattern for test data generation Introduces builder pattern for creating test data with faker.js integration: - UserBuilder: generates user test data with unknown types for flexible validation testing - QuestionBuilder: generates question test data with faker.js content and titles - AnswerBuilder: generates answer test data with faker.js content All builders accept unknown types as parameters to support validation testing scenarios.

---
+++ tests/builders/user.builder.ts
@@ -0,0 +1,37 @@
+import { faker } from '@faker-js/faker'
+
+export interface UserTestData {
+  id?: string
+  name?: unknown
+  email?: unknown
+  password?: unknown
+}
+
+export class UserBuilder {
+  private userData: UserTestData = {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    password: 'P@ssword123'
+  }
+
+  withName (name: unknown = faker.person.fullName()): UserBuilder {
+    this.userData.name = name
+    return this
+  }
+
+  withEmail (email: unknown = faker.internet.email()): UserBuilder {
+    this.userData.email = email
+    return this
+  }
+
+  withPassword (password: unknown = 'P@ssword123'): UserBuilder {
+    this.userData.password = password
+    return this
+  }
+
+  build (): UserTestData {
+    return { ...this.userData }
+  }
+}
+
+export const aUser = (): UserBuilder => new UserBuilder()
