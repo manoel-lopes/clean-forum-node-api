aab81b7c1e1fb7ef4061343158d795320304b4d0
manoel-lopes <manoel.lopes.full@gmail.com>
2025-09-01 14:27:30 -0300

    test(e2e): update existing e2e tests for comment integration Updates existing e2e tests to work with comment functionality integration, maintaining compatibility with the expanded comment system.

--- tests/e2e/comment-on-answer.e2e.test.ts
+++ tests/e2e/comment-on-answer.e2e.test.ts
@@ -1,7 +1,7 @@
 import type { Question } from '@/domain/entities/question/question.entity'
 import { anAnswer } from '../builders/answer.builder'
-import { aUser } from '../builders/user.builder'
 import { aQuestion } from '../builders/question.builder'
+import { aUser } from '../builders/user.builder'
 import { commentOnAnswer, createAnswer } from '../helpers/answer-helpers'
 import { createTestApp } from '../helpers/app-factory'
 import {
