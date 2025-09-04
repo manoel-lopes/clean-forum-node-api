8be8fab2b4d3c3ea3d9b839595d93492c15d99e3
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-09-03 23:14:52 -0300

    feat(main): update factories and route registrations for email validation Updates authentication and user creation factories, and integrates email validation routes into the users routing module.

--- src/main/factories/create-account.ts
+++ src/main/factories/create-account.ts
@@ -1,12 +1,12 @@
 import type { WebController } from '@/core/presentation/web-controller'
-import { PasswordHasherStub } from '@/infra/adapters/security/stubs/password-hasher.stub'
+import { BcryptPasswordHasher } from '@/infra/adapters/security/bcrypt-password-hasher'
 import { CachedRepositoriesFactory } from '@/infra/persistence/factories/cached-repositories.factory'
 import { CreateAccountUseCase } from '@/application/usecases/create-account/create-account.usecase'
 import { CreateAccountController } from '@/presentation/controllers/create-account/create-account.controller'
 
 export function makeCreateAccountController (): WebController {
   const usersRepository = CachedRepositoriesFactory.createUsersRepository()
-  const passwordHasher = new PasswordHasherStub()
+  const passwordHasher = new BcryptPasswordHasher()
   const createAccountUseCase = new CreateAccountUseCase(usersRepository, passwordHasher)
   return new CreateAccountController(createAccountUseCase)
 }
