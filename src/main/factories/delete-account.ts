eadc2eb0ca6216f3123d3799fdf3b42e84e95c69
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-09-05 15:23:34 -0300

    feat(cache): add caching to repositories

--- src/main/factories/delete-account.ts
+++ src/main/factories/delete-account.ts
@@ -1,12 +1,11 @@
 import type { WebController } from '@/core/presentation/web-controller'
 import { CachedRepositoriesFactory } from '@/infra/persistence/factories/cached-repositories.factory'
-import { PrismaRefreshTokensRepository } from '@/infra/persistence/repositories/prisma/prisma-refresh-tokens.repository'
 import { DeleteAccountUseCase } from '@/application/usecases/delete-account/delete-account.usecase'
 import { DeleteAccountController } from '@/presentation/controllers/delete-account/delete-account.controller'
 
 export function makeDeleteAccountController (): WebController {
   const usersRepository = CachedRepositoriesFactory.createUsersRepository()
-  const refreshTokensRepository = new PrismaRefreshTokensRepository()
+  const refreshTokensRepository = CachedRepositoriesFactory.createRefreshTokensRepository()
   const deleteAccountUseCase = new DeleteAccountUseCase(usersRepository, refreshTokensRepository)
   return new DeleteAccountController(deleteAccountUseCase)
 }
