8a51eff5d12038db15e3cc5579df880f325ff593
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-09-05 15:23:57 -0300

    refactor(factory): revert to non-cached repositories for some factories

--- src/main/factories/fetch-questions.ts
+++ src/main/factories/fetch-questions.ts
@@ -1,8 +1,8 @@
-import { CachedRepositoriesFactory } from '@/infra/persistence/factories/cached-repositories.factory'
+import { PrismaQuestionsRepository } from '@/infra/persistence/repositories/prisma/prisma-questions.repository'
 import { FetchQuestionsController } from '@/presentation/controllers/fetch-questions/fetch-questions.controller'
 
 export const makeFetchQuestionsController = (): FetchQuestionsController => {
-  const questionsRepository = CachedRepositoriesFactory.createQuestionsRepository()
+  const questionsRepository = new PrismaQuestionsRepository()
   const fetchQuestionsController = new FetchQuestionsController(questionsRepository)
   return fetchQuestionsController
 }
