56f3b2cfe1316608aa421521f5d855ec951e200f
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-09-05 17:17:58 -0300

    refactor(verify-email-validation): return 204 on success

--- src/presentation/controllers/verify-email-validation/verify-email-validation.controller.ts
+++ src/presentation/controllers/verify-email-validation/verify-email-validation.controller.ts
@@ -3,7 +3,7 @@ import type { UseCase } from '@/core/application/use-case'
 import type { HttpRequest, HttpResponse } from '@/infra/http/ports/http-protocol'
 import { EmailValidationNotFoundError } from '@/application/usecases/verify-email-validation/errors/email-validation-not-found.error'
 import { ExpiredValidationCodeError } from '@/application/usecases/verify-email-validation/errors/expired-validation-code.error'
-import { badRequest, notFound, ok } from '@/presentation/helpers/http-helpers'
+import { badRequest, noContent, notFound } from '@/presentation/helpers/http-helpers'
 
 export class VerifyEmailValidationController implements WebController {
   constructor (private readonly verifyEmailValidationUseCase: UseCase) {}
@@ -11,8 +11,8 @@ export class VerifyEmailValidationController implements WebController {
   async handle (req: HttpRequest): Promise<HttpResponse> {
     try {
       const { email, code } = req.body
-      const result = await this.verifyEmailValidationUseCase.execute({ email, code })
-      return ok(result)
+      await this.verifyEmailValidationUseCase.execute({ email, code })
+      return noContent()
     } catch (error) {
       if (error instanceof EmailValidationNotFoundError) {
         return notFound(error)
