ea9104d5e78a3eeec6d07b1e1feab6489ca23c63
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-09-07 22:39:16 -0300

    feat(routes): register fetch comment routes in resource routes Add fetchAnswerCommentsRoute and fetchQuestionCommentsRoute to their respective resource route files to provide proper REST endpoints at /answers/:answerId/comments and /questions/:questionId/comments.

--- src/main/fastify/routes/answers.routes.ts
+++ src/main/fastify/routes/answers.routes.ts
@@ -4,12 +4,14 @@ import { ensureAuthenticated } from '../middlewares/ensure-authenticated'
 import { answerQuestionRoute } from './answers/answer-question.route'
 import { commentOnAnswerRoute } from './answers/comment-on-answer.route'
 import { deleteAnswerRoute } from './answers/delete-answer.route'
+import { fetchAnswerCommentsRoute } from './comments/fetch-answer-comments.route'
 
 export async function answersRoutes (app: FastifyInstance) {
   registerRoutes(app, [
     answerQuestionRoute,
     deleteAnswerRoute,
-    commentOnAnswerRoute
+    commentOnAnswerRoute,
+    fetchAnswerCommentsRoute
   ], {
     preHandler: [ensureAuthenticated]
   })
