a68064447afad8a6c3b32ce642160ee37e6e9559
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-10-03 11:52:16 -0300

    refactor(domain): update entity props imports to use shared types

--- src/domain/entities/refresh-token/ports/refresh-token.props.ts
+++ src/domain/entities/refresh-token/ports/refresh-token.props.ts
@@ -1,4 +1,4 @@
-import type { Optional } from '@/util/types/optional'
+import type { Optional } from '@/shared/types/common/optional'
 import type { RefreshToken } from '../refresh-token.entity'
 
 export type RefreshTokenProps = Optional<Omit<RefreshToken, 'id'>, 'createdAt' | 'updatedAt' | 'expiresAt'>
