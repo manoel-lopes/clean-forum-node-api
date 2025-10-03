df1aba15eb20178d64dfe4833d620ab3f3a14cbc
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-10-03 13:36:06 -0300

    style(domain): organize imports in entity props files

--- src/domain/entities/question/ports/question.props.ts
+++ src/domain/entities/question/ports/question.props.ts
@@ -1,4 +1,4 @@
-import type { Optional } from '@/shared/types/common/optional'
 import type { Question } from '../question.entity'
+import type { Optional } from '@/shared/types/common/optional'
 
 export type QuestionProps = Optional<Omit<Question, 'id' | 'slug' | 'answers'>, 'createdAt' | 'updatedAt' | 'bestAnswerId'>
