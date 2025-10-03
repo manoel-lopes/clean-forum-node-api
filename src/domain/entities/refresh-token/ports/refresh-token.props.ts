fa7dc3641a892b3d027d518e72e3a68deb1bf1c8
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-10-03 13:36:06 -0300

    style(domain): organize imports in entity props files

--- src/domain/entities/refresh-token/ports/refresh-token.props.ts
+++ src/domain/entities/refresh-token/ports/refresh-token.props.ts
@@ -1,4 +1,4 @@
-import type { Optional } from '@/shared/types/common/optional'
 import type { RefreshToken } from '../refresh-token.entity'
+import type { Optional } from '@/shared/types/common/optional'
 
 export type RefreshTokenProps = Optional<Omit<RefreshToken, 'id'>, 'createdAt' | 'updatedAt' | 'expiresAt'>
