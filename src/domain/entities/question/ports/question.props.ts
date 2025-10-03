5267dda0bac80991a67041be810a7143c4b5b81f
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-10-03 11:52:16 -0300

    refactor(domain): update entity props imports to use shared types

--- src/domain/entities/question/ports/question.props.ts
+++ src/domain/entities/question/ports/question.props.ts
@@ -1,4 +1,4 @@
-import type { Optional } from '@/util/types/optional'
+import type { Optional } from '@/shared/types/common/optional'
 import type { Question } from '../question.entity'
 
 export type QuestionProps = Optional<Omit<Question, 'id' | 'slug' | 'answers'>, 'createdAt' | 'updatedAt' | 'bestAnswerId'>
