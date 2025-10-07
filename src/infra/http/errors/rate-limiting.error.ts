a779dba178ba510ce05447cfffcf0b61d7ff0187
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-10-07 12:04:15 -0300

    feat(http): add generic rate limiting error

---
+++ src/infra/http/errors/rate-limiting.error.ts
@@ -0,0 +1,11 @@
+export type RateLimitingErrorCode = | 'AUTH_RATE_LIMIT_EXCEEDED' | 'USER_CREATION_RATE_LIMIT_EXCEEDED' | 'EMAIL_VALIDATION_RATE_LIMIT_EXCEEDED'
+
+export class RateLimitingError extends Error {
+  public readonly code: RateLimitingErrorCode
+
+  constructor (code: RateLimitingErrorCode, message: string) {
+    super(message)
+    this.code = code
+    this.name = 'RateLimitingError'
+  }
+}
