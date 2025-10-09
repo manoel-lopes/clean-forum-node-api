d1b8700ce7cb193da772588b829121c8fa9a9e63
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-10-09 19:36:35 -0300

    refactor(domain): simplify entity props

--- src/domain/enterprise/entities/answer.entity.ts
+++ src/domain/enterprise/entities/answer.entity.ts
@@ -1,8 +1,7 @@
 import type { Entity } from '@/core/domain/entity'
-import type { Optional } from '@/shared/types/common/optional'
 import type { Props } from '@/shared/types/custom/props'
 
-export type AnswerProps = Optional<Props<Answer>, 'excerpt'>
+export type AnswerProps = Props<Answer>
 
 export interface Answer extends Entity {
   content: string
