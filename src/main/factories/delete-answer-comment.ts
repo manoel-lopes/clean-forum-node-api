a8d548ceac7c6e93b0004ed154516c6f8ac44ed2
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-10-10 10:37:39 -0300

    refactor(factories): update delete answer comment factory to use specific use case

--- src/main/factories/delete-answer-comment.ts
+++ src/main/factories/delete-answer-comment.ts
@@ -1,12 +1,11 @@
 import type { WebController } from '@/core/presentation/web-controller'
-import { DeleteCommentUseCase } from '@/domain/application/usecases/delete-comment/delete-comment.usecase'
+import { DeleteAnswerCommentUseCase } from '@/domain/application/usecases/delete-answer-comment/delete-answer-comment.usecase'
 import { CachedRepositoriesFactory } from '@/infra/persistence/factories/cached-repositories.factory'
 import { DeleteCommentController } from '@/presentation/controllers/delete-comment/delete-comment.controller'
 
 export function makeDeleteAnswerCommentController (): WebController {
-  const commentsRepository = CachedRepositoriesFactory.createAnswerCommentsRepository()
-  const questionsRepository = CachedRepositoriesFactory.createQuestionsRepository()
+  const answerCommentsRepository = CachedRepositoriesFactory.createAnswerCommentsRepository()
   const answersRepository = CachedRepositoriesFactory.createAnswersRepository()
-  const deleteCommentUseCase = new DeleteCommentUseCase(commentsRepository, questionsRepository, answersRepository)
-  return new DeleteCommentController(deleteCommentUseCase)
+  const deleteAnswerCommentUseCase = new DeleteAnswerCommentUseCase(answerCommentsRepository, answersRepository)
+  return new DeleteCommentController(deleteAnswerCommentUseCase)
 }
