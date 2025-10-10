49f81145005d4e1e52f33ed446a11fba3fa41aff
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-10-10 19:55:43 -0300

    fix(test): set future expiration date in makeEmailValidation factory

--- src/shared/util/factories/domain/make-email-validation.ts
+++ src/shared/util/factories/domain/make-email-validation.ts
@@ -3,11 +3,13 @@ import { faker } from '@faker-js/faker'
 import type { EmailValidation } from '@prisma/client'
 
 export function makeEmailValidation (override: Partial<EmailValidation> = {}): EmailValidation {
+  const futureDate = new Date()
+  futureDate.setMinutes(futureDate.getMinutes() + 10)
   return {
     id: uuidv7(),
     email: faker.internet.email(),
     code: faker.string.uuid(),
-    expiresAt: new Date(),
+    expiresAt: futureDate,
     isVerified: false,
     createdAt: new Date(),
     updatedAt: new Date(),
