acd9a974dea4e889c4dec7c9d4e9ee1c12009f35
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-10-15 19:13:10 -0300

    feat(factories): add attachment and update factories

---
+++ src/main/factories/update-answer.ts
@@ -0,0 +1,10 @@
+import type { WebController } from '@/core/presentation/web-controller'
+import { UpdateAccountUseCase } from '@/domain/application/usecases/update-answer/update-answer.usecase'
+import { CachedRepositoriesFactory } from '@/infra/persistence/factories/cached-repositories.factory'
+import { UpdateAnswerController } from '@/presentation/controllers/update-answer/update-answer.controller'
+
+export function makeUpdateAnswerController (): WebController {
+  const answersRepository = CachedRepositoriesFactory.createAnswersRepository()
+  const updateAnswerUseCase = new UpdateAccountUseCase(answersRepository)
+  return new UpdateAnswerController(updateAnswerUseCase)
+}
