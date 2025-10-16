ad947b1692fbfc27298eb743f26ee438bb518b97
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-10-16 14:00:24 -0300

    feat(routes): register attachment and update routes

--- src/main/fastify/routes/users.routes.ts
+++ src/main/fastify/routes/users.routes.ts
@@ -3,6 +3,7 @@ import { registerRoutes } from '@/main/fastify/helpers/register-routes'
 import { ensureAuthenticated } from '@/main/fastify/middlewares/ensure-authenticated'
 import { createAccountRoute } from './users/create-account.route'
 import { deleteAccountRoute } from './users/delete-account.route'
+import { fetchUserQuestionsRoute } from './users/fetch-user-questions.route'
 import { fetchUsersRoute } from './users/fetch-users.route'
 import { getUserByEmailRoute } from './users/get-user-by-email.route'
 import { sendEmailValidationRoute } from './users/send-email-validation.route'
@@ -17,7 +18,8 @@ export async function usersRoutes (app: FastifyInstance) {
   registerRoutes(app, [
     getUserByEmailRoute,
     deleteAccountRoute,
-    fetchUsersRoute
+    fetchUsersRoute,
+    fetchUserQuestionsRoute
   ], {
     preHandler: [ensureAuthenticated]
   })
