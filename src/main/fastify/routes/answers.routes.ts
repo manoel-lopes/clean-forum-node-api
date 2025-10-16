6fa01401fe66c3f88f1e95030ff1fe8bcd167476
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-10-16 14:00:24 -0300

    feat(routes): register attachment and update routes

--- src/main/fastify/routes/answers.routes.ts
+++ src/main/fastify/routes/answers.routes.ts
@@ -4,14 +4,24 @@ import { ensureAuthenticated } from '../middlewares/ensure-authenticated'
 import { answerQuestionRoute } from './answers/answer-question.route'
 import { commentOnAnswerRoute } from './answers/comment-on-answer.route'
 import { deleteAnswerRoute } from './answers/delete-answer.route'
+import { updateAnswerRoute } from './answers/update-answer.route'
+import { attachToAnswerRoute } from './attachments/attach-to-answer.route'
+import { deleteAnswerAttachmentRoute } from './attachments/delete-answer-attachment.route'
+import { fetchAnswerAttachmentsRoute } from './attachments/fetch-answer-attachments.route'
+import { updateAnswerAttachmentRoute } from './attachments/update-answer-attachment.route'
 import { fetchAnswerCommentsRoute } from './comments/fetch-answer-comments.route'
 
 export async function answersRoutes (app: FastifyInstance) {
   registerRoutes(app, [
     answerQuestionRoute,
     deleteAnswerRoute,
+    updateAnswerRoute,
     commentOnAnswerRoute,
-    fetchAnswerCommentsRoute
+    fetchAnswerCommentsRoute,
+    attachToAnswerRoute,
+    fetchAnswerAttachmentsRoute,
+    updateAnswerAttachmentRoute,
+    deleteAnswerAttachmentRoute
   ], {
     preHandler: [ensureAuthenticated]
   })
--- src/main/fastify/routes/questions.routes.ts
+++ src/main/fastify/routes/questions.routes.ts
@@ -1,6 +1,10 @@
 import type { FastifyInstance } from 'fastify'
 import { registerRoutes } from '@/main/fastify/helpers/register-routes'
 import { ensureAuthenticated } from '../middlewares/ensure-authenticated'
+import { attachToQuestionRoute } from './attachments/attach-to-question.route'
+import { deleteQuestionAttachmentRoute } from './attachments/delete-question-attachment.route'
+import { fetchQuestionAttachmentsRoute } from './attachments/fetch-question-attachments.route'
+import { updateQuestionAttachmentRoute } from './attachments/update-question-attachment.route'
 import { fetchQuestionCommentsRoute } from './comments/fetch-question-comments.route'
 import { chooseQuestionBestAnswerRoute } from './questions/choose-question-best-answer.route'
 import { commentOnQuestionRoute } from './questions/comment-on-question.route'
@@ -8,16 +12,22 @@ import { createQuestionRoute } from './questions/create-question.route'
 import { deleteQuestionRoute } from './questions/delete-question.route'
 import { fetchQuestionsRoute } from './questions/fetch-questions.route'
 import { getQuestionBySlugRoute } from './questions/get-question-by-slug.route'
+import { updateQuestionRoute } from './questions/update-question.route'
 
 export async function questionsRoutes (app: FastifyInstance) {
   registerRoutes(app, [
     createQuestionRoute,
     deleteQuestionRoute,
     getQuestionBySlugRoute,
+    updateQuestionRoute,
     chooseQuestionBestAnswerRoute,
     fetchQuestionsRoute,
     commentOnQuestionRoute,
-    fetchQuestionCommentsRoute
+    fetchQuestionCommentsRoute,
+    attachToQuestionRoute,
+    fetchQuestionAttachmentsRoute,
+    updateQuestionAttachmentRoute,
+    deleteQuestionAttachmentRoute
   ], {
     preHandler: [ensureAuthenticated]
   })
