92fbd4f22792f8861b8f24e0e1798795d90b0395
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-10-16 14:00:04 -0300

    feat(factory): add attachment repository factory methods

--- src/infra/persistence/factories/cached-repositories.factory.ts
+++ src/infra/persistence/factories/cached-repositories.factory.ts
@@ -1,14 +1,18 @@
 import { RedisService } from '@/infra/providers/cache/redis-service'
+import { CachedAnswerAttachmentsRepository } from '../repositories/cached/cached-answer-attachments.repository'
 import { CachedAnswerCommentsRepository } from '../repositories/cached/cached-answer-comments.repository'
 import { CachedAnswersRepository } from '../repositories/cached/cached-answers.repository'
 import { CachedEmailValidationsRepository } from '../repositories/cached/cached-email-validations.repository'
+import { CachedQuestionAttachmentsRepository } from '../repositories/cached/cached-question-attachments.repository'
 import { CachedQuestionCommentsRepository } from '../repositories/cached/cached-question-comments.repository'
 import { CachedQuestionsRepository } from '../repositories/cached/cached-questions.repository'
 import { CachedRefreshTokensRepository } from '../repositories/cached/cached-refresh-tokens.repository'
 import { CachedUsersRepository } from '../repositories/cached/cached-users.repository'
+import { PrismaAnswerAttachmentsRepository } from '../repositories/prisma/prisma-answer-attachments.repository'
 import { PrismaAnswerCommentsRepository } from '../repositories/prisma/prisma-answer-comments.repository'
 import { PrismaAnswersRepository } from '../repositories/prisma/prisma-answers.repository'
 import { PrismaEmailValidationsRepository } from '../repositories/prisma/prisma-email-validations.repository'
+import { PrismaQuestionAttachmentsRepository } from '../repositories/prisma/prisma-question-attachments.repository'
 import { PrismaQuestionCommentsRepository } from '../repositories/prisma/prisma-question-comments.repository'
 import { PrismaQuestionsRepository } from '../repositories/prisma/prisma-questions.repository'
 import { PrismaRefreshTokensRepository } from '../repositories/prisma/prisma-refresh-tokens.repository'
@@ -57,5 +61,15 @@ export abstract class CachedRepositoriesFactory {
   static createEmailValidationsRepository () {
     const emailValidationsRepository = new PrismaEmailValidationsRepository()
     return new CachedEmailValidationsRepository(this.getRedis(), emailValidationsRepository)
+  }
+
+  static createQuestionAttachmentsRepository () {
+    const questionAttachmentsRepository = new PrismaQuestionAttachmentsRepository()
+    return new CachedQuestionAttachmentsRepository(this.getRedis(), questionAttachmentsRepository)
+  }
+
+  static createAnswerAttachmentsRepository () {
+    const answerAttachmentsRepository = new PrismaAnswerAttachmentsRepository()
+    return new CachedAnswerAttachmentsRepository(this.getRedis(), answerAttachmentsRepository)
   }
 }
